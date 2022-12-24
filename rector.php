44b791837bce5a0e4a6dcc8e0e462183f03ed9f4
Dante <dantesabatier@me.com>
2022-12-24 09:00:26 -0600

    Update rector.php

--- rector.php
+++ rector.php
@@ -17,17 +17,22 @@ use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src'
+        __DIR__ . "/src"
     ]);
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
         SetList::CODE_QUALITY,
     ]);
     $rectorConfig->skip([
-        JsonThrowOnErrorRector::class => [
-            __DIR__ . '/src/Service.php'
-        ],
         ExplicitBoolCompareRector::class,
+        JsonThrowOnErrorRector::class => [
+            __DIR__ . "/src/Responder.php",
+            __DIR__ . "/src/Service.php",
+        ],
+        ReadOnlyPropertyRector::class => [
+            __DIR__ . "src/Action.php",
+            __DIR__ . "src/Endpoint.php",
+        ],
         ReturnNeverTypeRector::class,
         NullToStrictStringFuncCallArgRector::class,
         UnionTypesRector::class,

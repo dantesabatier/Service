a0327503bda202944adae07ebcf2941540b8982d
Dante <dantesabatier@me.com>
2023-01-07 00:16:54 -0600

    Update BearerProtectionSpace.php

--- src/BearerProtectionSpace.php
+++ src/BearerProtectionSpace.php
@@ -28,7 +28,9 @@ class BearerProtectionSpace extends ProtectionSpace
     public function __construct()
     {
         parent::__construct();
+        $this->allowedMethods = new ArrayClass([HTTPRequestMethod::get, HTTPRequestMethod::post, HTTPRequestMethod::options]);
         if ($this->request->httpMethod != HTTPRequestMethod::options) {
+            $this->contentType = "application/json; charset=utf-8";
             $this->defaultAuthenticationMethod = self::authenticationMethodBearer;
             if ($this->authenticationMethod != $this->defaultAuthenticationMethod) {
                 throw new UnauthorizedException();
@@ -36,8 +38,6 @@ class BearerProtectionSpace extends ProtectionSpace
             $this->token = ($authentication = $this->request->valueForHttpHeaderField("Authorization")) && ($authenticationIndex = (int)strpos($authentication, " ")) && (($hash = trim(substring_from_index($authentication, $authenticationIndex))) && count(explode(".", $hash)) == 3) ? new JSONWebToken(ProcessInfo::processInfo()->environment["APPLICATION_TOKEN_KEY"] ?? fatal_error("Environment variable \"APPLICATION_TOKEN_KEY\" cannot be null"), null, $hash, $this->request->url->host) : null;
             $this->username = $this->token?->payload?->username;
             $this->isProtectedContentAvailable = (bool)$this->token?->isValid;
-            $this->allowedMethods = new ArrayClass([HTTPRequestMethod::get, HTTPRequestMethod::post, HTTPRequestMethod::options]);
-            $this->contentType = "application/json; charset=utf-8";
         }
     }
 

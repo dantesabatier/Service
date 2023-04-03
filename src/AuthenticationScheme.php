0f60501e2fb27a414ade4c45caa10d65ec3c7b73
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-03 14:53:11 -0500

    Update AuthenticationScheme.php

--- src/AuthenticationScheme.php
+++ src/AuthenticationScheme.php
@@ -6,5 +6,11 @@ enum AuthenticationScheme: string
 {
     case basic = "Basic";
     case digest = "Digest";
+    case hoba = "HOBA";
+    case mutual = "Mutual";
+    case negotiate = "Negotiate";
+    case vapid = "VAPID";
+    case scram = "SCRAM";
+    case aws = "AWS4-HMAC-SHA256";
     case bearer = "Bearer";
 }

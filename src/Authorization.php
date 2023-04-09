8006655e2a7450a160f123fabece5ac339fd1f62
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-09 10:03:33 -0500

    Update Authorization.php

--- src/Authorization.php
+++ src/Authorization.php
@@ -3,24 +3,25 @@
 namespace Sabatier\Service;
 
 use Exception;
-use Sabatier\CoreData\EntityDescription;
-use Sabatier\CoreData\FetchRequest;
 use Sabatier\CoreData\ManagedObject;
+use Sabatier\Foundation\ArrayClass;
+use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\Networking\URLCredential;
 use Sabatier\Foundation\Predicates\ComparisonPredicate;
 use Sabatier\Foundation\Predicates\Expression;
-use Sabatier\Foundation\ProcessInfo;
 use Sabatier\Foundation\UndefinedKeyException;
 
 readonly class Authorization
 {
     public ?URLCredential $credential;
     public ?ManagedObject $user;
+    public bool $isValid;
 
     public function __construct(public AuthenticationScheme $scheme, public string $parametersView)
     {
         unset($this->credential);
         unset($this->user);
+        unset($this->isValid);
     }
 
     public function __get(string $name)
@@ -36,6 +37,35 @@ readonly class Authorization
                         [$username, $password] = $components;
                         return new URLCredential($username, $password);
                     })(),
+                    AuthenticationScheme::digest => (function (): ?URLCredential {
+                        /** @var Dictionary<string> $parameters */
+                        $parameters = (new ArrayClass(explode(",", $this->parametersView)))->reduce(new Dictionary(), function (Dictionary $result, string $e): Dictionary {
+                            $components = array_map(fn(string $e): string => trim($e), explode("=", $e));
+                            if (count($components) === 2) {
+                                [$name, $value] = $components;
+                                $result[$name] = $value;
+                            }
+                            return $result;
+                        });
+                        if (!($username = $parameters["username"]) || !($uri = $parameters["uri"]) || !($nonce = $parameters["nonce"]) || !($nc = $parameters["nc"]) || !($cnonce = $parameters["cnonce"]) || !($qop = $parameters["qop"])) {
+                            return null;
+                        }
+                        if (!($user = $this->fetch($username))) {
+                            return null;
+                        }
+                        $password = $user->valueForKey("password");
+                        $application = Application::shared();
+                        $request = $application->request;
+                        $realm = $request->url->host;
+                        $A1 = md5("$username:$realm:$password");
+                        $A2 = md5("$request->httpMethod:$uri");
+                        $validResponse = md5("$A1:$nonce:$nc:$cnonce:$qop:$A2");
+                        if ($parameters["response"] !== $validResponse) {
+                            return null;
+                        }
+                        $this->user = $user;
+                        return new URLCredential($username, $password);
+                    })(),
                     default => null
                 };
             })(),
@@ -43,25 +73,25 @@ readonly class Authorization
                 if (!($credential = $this->credential)) {
                     return null;
                 }
-                $username = $credential->user;
-                $application = Application::shared();
-                $serialization = $application->serialization;
-                $context = $application->persistentContainer->viewContext;
-                $processInfo = ProcessInfo::processInfo();
-                $environment = $processInfo->environment;
-                /** @var string $entityName */
-                $entityName = $environment["USER_ENTITY_NAME"] ?? "User";
-                /** @var FetchRequest<ManagedObject> $fetchRequest */
-                $fetchRequest = new FetchRequest();
-                $fetchRequest->entity = EntityDescription::entity($entityName, $context);
-                $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($username));
-                try {
-                    return $context->fetch($fetchRequest)->first()?->serialized($serialization);
-                } catch (Exception) {
-                    return null;
-                }
+                return $this->fetch($credential->user);
             })(),
+            "isValid" => ($credential = $this->credential) && ($user = $this->user) && password_verify($credential->password, $user->valueForKey("password")),
             default => throw new UndefinedKeyException("<Authorization is not key value coding compliant for the key \"$name\"")
         };
+    }
+
+    private function fetch(string $username): ?ManagedObject
+    {
+        $application = Application::shared();
+        $context = $application->persistentContainer->viewContext;
+        /** @var class-string<ManagedObject> $managedObjectClass */
+        $managedObjectClass = "App\Model\User";
+        $fetchRequest = $managedObjectClass::fetchRequest();
+        $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($username));
+        try {
+            return $context->fetch($fetchRequest)->first();
+        } catch (Exception) {
+            return null;
+        }
     }
 }

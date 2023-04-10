be086bc0bb715899d3c3ce0b5628f3b1488be8d0
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-09 20:06:23 -0500

    Update Authorization.php

--- src/Authorization.php
+++ src/Authorization.php
@@ -6,33 +6,39 @@ use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\Networking\URLCredential;
 use Sabatier\Foundation\UndefinedKeyException;
+use function Sabatier\Foundation\substring_from_index;
+use function Sabatier\Foundation\substring_to_index;
 
 /** @internal */
 readonly class Authorization
 {
     public ?URLCredential $credential;
     /** @var Dictionary<string> */
-    private Dictionary $parameters;
+    public Dictionary $parameters;
+    private string $name;
+    private string $parametersView;
 
-    public function __construct(private AuthenticationScheme $scheme, private string $parametersView)
+    public function __construct(public string $rawValue)
     {
-        unset($this->credential);
+        unset($this->name);
+        unset($this->parametersView);
         unset($this->parameters);
+        unset($this->credential);
     }
 
     public function __get(string $name)
     {
         return $this->$name = match ($name) {
+            "name" => trim(substring_to_index($this->rawValue, (int)strpos($this->rawValue, " "))),
+            "parametersView" => trim(substring_from_index($this->rawValue, (int)strpos($this->rawValue, " "))),
             "parameters" => (new ArrayClass(explode(",", $this->parametersView)))->reduce(new Dictionary(), function (Dictionary $result, string $e): Dictionary {
-                $components = explode("=", $e);
-                if (count($components) === 2) {
-                    [$name, $value] = $components;
-                    $result[trim($name)] = trim($value);
-                }
+                $components = explode("=", $e, 2);
+                $result[trim($components[0])] = count($components) > 1 ? trim($components[1]) : "";
                 return $result;
             }),
             "credential" => (function (): ?URLCredential {
-                return match ($this->scheme) {
+                $scheme = AuthenticationScheme::tryFrom($this->name);
+                return match ($scheme) {
                     AuthenticationScheme::basic => (function (): ?URLCredential {
                         $components = explode(":", base64_decode($this->parametersView));
                         if (count($components) !== 2) {
@@ -51,28 +57,6 @@ readonly class Authorization
                 };
             })(),
             default => throw new UndefinedKeyException("<Authorization is not key value coding compliant for the key \"$name\"")
-        };
-    }
-
-    public function perform(Authentication $authentication): bool
-    {
-        if (!($credential = $authentication->credential) || !($user = $authentication->user)) {
-            return false;
-        }
-        $password = $user->valueForKey("password");
-        return match ($this->scheme) {
-            AuthenticationScheme::basic => password_verify((string)$credential->password, $password),
-            AuthenticationScheme::digest => (function () use ($authentication, $password): bool {
-                $parameters = $this->parameters;
-                if (!($username = $parameters["username"]) || !($uri = $parameters["uri"]) || !($nonce = $parameters["nonce"]) || !($nc = $parameters["nc"]) || !($cnonce = $parameters["cnonce"]) || !($qop = $parameters["qop"])) {
-                    return false;
-                }
-                $A1 = md5("$username:{$authentication->request->url->host}:$password");
-                $A2 = md5("{$authentication->request->httpMethod}:$uri");
-                $validResponse = md5("$A1:$nonce:$nc:$cnonce:$qop:$A2");
-                return $parameters["response"] === $validResponse;
-            })(),
-            default => false
         };
     }
 }

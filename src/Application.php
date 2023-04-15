27b8e145e9b7d6a4e83974323d1cb21fe93fba15
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-14 20:27:26 -0600

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -307,7 +307,7 @@ class Application extends Responder
             throw new UnauthorizedException();
         }
         $this->persistentContainer->viewContext->transactionAuthor = match ($this->request->httpMethod) {
-            HTTPRequestMethod::post, HTTPRequestMethod::put, HTTPRequestMethod::patch, HTTPRequestMethod::delete => $_SESSION["user"],
+            HTTPRequestMethod::post, HTTPRequestMethod::put, HTTPRequestMethod::patch, HTTPRequestMethod::delete => $_SESSION["user"] ?? null,
             default => null
         };
         return $responder;

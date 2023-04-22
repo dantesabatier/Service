37c539c5a5932abbc196fc90feb183d604cfca2a
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-22 16:08:10 -0600

    Update CookieParameters.php

--- src/CookieParameters.php
+++ src/CookieParameters.php
@@ -2,6 +2,7 @@
 
 namespace Sabatier\Service;
 
+use JetBrains\PhpStorm\ArrayShape;
 use JetBrains\PhpStorm\ExpectedValues;
 use Sabatier\Foundation\Networking\HTTPCookieStringPolicy;
 use Sabatier\Foundation\URLComponents;
@@ -9,11 +10,11 @@ use function Sabatier\Foundation\request_url;
 
 readonly class CookieParameters
 {
+    #[ArrayShape(["lifetime" => "int", "path" => "string", "domain" => "string", "secure" => "bool", "httponly" => "bool", "samesite" => "string"])]
     public array $allValues;
 
     public function __construct(public string $domain, public string $path = "/", public int $lifetime = 0, public bool $isSecure = true, public bool $isHTTPOnly = true, #[ExpectedValues(valuesFromClass: HTTPCookieStringPolicy::class)] public string $sameSitePolicy = HTTPCookieStringPolicy::sameSiteLax)
     {
-        /** @noinspection SpellCheckingInspection */
         $this->allValues = ["lifetime" => $this->lifetime, "path" => $this->path, "domain" => $this->domain, "secure" => $this->isSecure, "httponly" => $this->isHTTPOnly, "samesite" => $this->sameSitePolicy];
     }
 

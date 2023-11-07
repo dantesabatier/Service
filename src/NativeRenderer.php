961d18c9ec5d059f4b0ecae83abb7d03d6769e34
Dante <dante.palacios@holatrabajo.com>
2023-11-07 09:51:23 -0600

    Update NativeRenderer.php

--- src/NativeRenderer.php
+++ src/NativeRenderer.php
@@ -6,7 +6,7 @@ class NativeRenderer extends Renderer
 {
     public function render(string $name, object|array $context): string
     {
-        $path = $this->bundle->url($name, "php")?->path ?? throw new NotFoundException("Template \"$name\" does not exists");
+        $path = $this->bundle->url($name, "php")?->path ?? throw new NotFoundException("The view named \"$name\" does not exists");
         $context = (array)$context;
         extract($context);
         ob_start();

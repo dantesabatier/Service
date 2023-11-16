b0c96c8c6bb9d23aee571f566aa8be43516a2a35
Dante <dante.palacios@holatrabajo.com>
2023-11-16 13:01:52 -0600

    Update Responder.php

--- src/Responder.php
+++ src/Responder.php
@@ -9,6 +9,8 @@ use Sabatier\CoreData\ManagedObjectContext;
 use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\CompareOptions;
 use Sabatier\Foundation\Dictionary;
+use Sabatier\Foundation\Error;
+use Sabatier\Foundation\ErrorRecoveryAttempting;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\Networking\HTTPURLResponse;
 use Sabatier\Foundation\Networking\URLRequest;
@@ -110,5 +112,35 @@ abstract class Responder extends ObjectClass
     public function response(): HTTPURLResponse
     {
         return new HTTPURLResponse($this->request->url);
+    }
+
+    /**
+     * Presents an error alert to the user as an application-modal dialog.
+     *
+     * The alert displays information found in the Error object $error; this information can include error description, recovery suggestion, failure reason, and button titles (all localized). The method returns true if error recovery succeeded and false otherwise. For error recovery to be attempted, a recovery-attempter object (that is, an object conforming to the {@see ErrorRecoveryAttempting} informal protocol) must be associated with error.
+     *
+     * The default implementation of this method sends {@see willPresentError()} to self. By doing this, Responder gives subclasses an opportunity to customize error presentation. It then forwards the message, passing any customized error object, to the next responder; if there is no next responder, it passes the error object to App, which displays a document-modal error alert. When the user dismisses the alert, any recovery attempter associated with the error object is given a chance to recover from the error. See the class description for the precise route up the responder chain (plus document and controller objects) this message might travel.
+     *
+     * It is not recommended that you attempt to override this method. If you wish to customize the error presentation, override {@see willPresentError()} instead.
+     * @param Error $error An object containing information about an error.
+     * @return bool
+     */
+    public function presentError(Error $error): bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns a custom version of the supplied error object that’s more suitable for presentation in alert sheets and dialogs.
+     *
+     * When overriding this method, you can examine error and, if its localized description or recovery information is unhelpfully generic, return an error object with more specific localized text. If you do this, always use the domain and error code of the Error object to distinguish between errors whose presentation you want to customize and those you don’t. Don’t make decisions based on the localized description, recovery suggestion, or recovery options because parsing localized text is problematic.
+     *
+     * The default implementation of this method returns error unchanged.
+     * @param Error $error The error object to customize.
+     * @return Error The customized error object; if you decide not to customize the error presentation, return by sending this message to super (that is, return parent::willPresentError($error)).
+     */
+    public function willPresentError(Error $error): Error
+    {
+        return $error;
     }
 }

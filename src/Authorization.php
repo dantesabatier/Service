14c9341befe9510c1d2cc65b7a5cf4a06e625c3d
Dante <dante.palacios@holatrabajo.com>
2023-11-27 13:44:26 -0600

    Update Authorization.php

--- src/Authorization.php
+++ src/Authorization.php
@@ -15,6 +15,17 @@ readonly class Authorization
         unset($this->parameters);
     }
 
+    public function __serialize(): array
+    {
+        return ["scheme" => $this->scheme, "credentials" => $this->credentials];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->scheme = $data["scheme"];
+        $this->credentials = $data["credentials"];
+    }
+
     public function __get(string $name)
     {
         $this->$name = match ($name) {

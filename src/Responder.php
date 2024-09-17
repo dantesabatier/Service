471a8cf4fe94e746c0bcbe343879b4b02dfaa2e6
dantesabatier <dantesabatier@me.com>
2024-09-16 19:53:20 -0600

    Update Responder.php

--- src/Responder.php
+++ src/Responder.php
@@ -90,14 +90,11 @@ abstract class Responder extends ObjectClass
                             $components = new URLComponents($other);
                             $other = "$components->path$components->query";
                         }
-                        if (string_is_equal($path, $other, CompareOptions::caseInsensitive)) {
-                            $ok = $attemptProceedingWithDefaultImplementation();
-                            if ($request->httpMethod === $action->method) {
-                                $this->content = json_encode([]);
-                                $this->contentType = "application/json; charset=utf-8";
-                                $this->perform($selector);
-                            }
-                            return $ok;
+                        if (string_is_equal($path, $other, CompareOptions::caseInsensitive) && $attemptProceedingWithDefaultImplementation() && ($request->httpMethod === $action->method)) {
+                            $this->content = json_encode([]);
+                            $this->contentType = "application/json; charset=utf-8";
+                            $this->perform($selector);
+                            return true;
                         }
                     }
                 }

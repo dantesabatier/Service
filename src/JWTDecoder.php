1d28801172616e974a3918b6d26fc3612ea5d9c6
dantesabatier <dantesabatier@me.com>
2024-09-24 11:42:20 -0600

    Create JWTDecoder.php

---
+++ src/JWTDecoder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sabatier\Service;
+
+use Sabatier\Foundation\Date;
+
+readonly class JWTDecoder
+{
+    public function __construct(private string $key, private ?string $issuer = null)
+    {
+    }
+
+    private function validate(string $token): bool
+    {
+        if (!str_contains($token, ".")) {
+            return false;
+        }
+        $components = explode(".", $token);
+        if (count($components) !== 3) {
+            return false;
+        }
+        [$header, $payload, $signature] = $components;
+        $unsigned = sprintf("%s.%s", $header, $payload);
+        $signed = base64_encode(hash_hmac("sha256", $unsigned, $this->key, true));
+        if ($signature !== $signed || !($obj = json_decode(base64_decode($payload)))) {
+            return false;
+        }
+        $date = new Date();
+        return !(((property_exists($obj, JWTNotBeforeField) && $obj->nbf > $date->timeIntervalSinceReferenceDate) || (property_exists($obj, JWTExpirationField) && $obj->exp < $date->timeIntervalSinceReferenceDate) || (property_exists($obj, JWTIssuerField) && $obj->iss !== $this->issuer)));
+    }
+
+    public function decode(string $data): ?array
+    {
+        if (!$this->validate($data)) {
+            return null;
+        }
+        [, $payload,] = explode(".", $data);
+        return json_decode(base64_decode($payload), true);
+    }
+}

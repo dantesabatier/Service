51a64e10aaae6edc986ca1b9a337a9129eeabc29
dantesabatier <dantesabatier@me.com>
2024-09-24 11:42:26 -0600

    Update Authentication.php

--- src/Authentication.php
+++ src/Authentication.php
@@ -5,12 +5,13 @@ namespace Sabatier\Service;
 use Exception;
 use Sabatier\CoreData\ManagedObject;
 use Sabatier\Foundation\ArrayClass;
+use Sabatier\Foundation\Date;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\Networking\URLCredential;
 use Sabatier\Foundation\Predicates\ComparisonPredicate;
 use Sabatier\Foundation\Predicates\Expression;
+use Sabatier\Foundation\UserDefaults;
 use function Sabatier\Foundation\is_password;
-use function Sabatier\Foundation\read_random;
 use function Sabatier\Foundation\substring_from_index;
 use function Sabatier\Foundation\substring_to_index;
 
@@ -53,10 +54,13 @@ class Authentication extends Responder
                     return new URLCredential($username);
                 })(),
                 AuthenticationScheme::bearer => (function (): ?URLCredential {
-                    if (!($user = $this->userBy("token", $this->authorization->credentials))) {
+                    /** @var string $key */
+                    $key = UserDefaults::standard()->string(JWTPrivateKey);
+                    $decoder = new JWTDecoder($key, $this->request->url->host);
+                    if (!($payload = $decoder->decode($this->authorization->credentials)) || !($username = $payload["username"])) {
                         return null;
                     }
-                    return new URLCredential($user->valueForKey("username"));
+                    return new URLCredential($username);
                 })()
             };
             return $this->$name;
@@ -65,7 +69,11 @@ class Authentication extends Responder
                 if (!($username = $this->credential?->user ?? Application::shared()->session->valueForKey("user"))) {
                     return null;
                 }
-                return $this->userBy("username", $username);
+                /** @var class-string<ManagedObject> $userClass */
+                $userClass = self::$userClass;
+                $fetchRequest = $userClass::fetchRequest();
+                $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($username));
+                return $this->managedObjectContext->fetch($fetchRequest)->first?->serialized($this->serialization);
             })();
             return $this->$name;
         } elseif ($name == "isProtectedContentAvailable") {
@@ -81,7 +89,15 @@ class Authentication extends Responder
                                 $response = hash("sha256", "$HA1:$nonce:$nc:$cnonce:$qop:$HA2");
                                 return $parameters["response"] === $response;
                             })(),
-                        AuthenticationScheme::bearer => $this->authorization->credentials === $user->valueForKey("token")
+                        AuthenticationScheme::bearer => (function () use ($user): bool {
+                            /** @var string $key */
+                            $key = UserDefaults::standard()->string(JWTPrivateKey);
+                            $decoder = new JWTDecoder($key, $this->request->url->host);
+                            if (!($payload = $decoder->decode($this->authorization->credentials)) || !($username = $payload["username"])) {
+                                return false;
+                            }
+                            return $user->valueForKey("username") === $username;
+                        })()
                     });
             return $this->$name;
         } elseif ($name == "allowedMethods") {
@@ -95,30 +111,24 @@ class Authentication extends Responder
     /**
      * @throws Exception
      */
-    private function userBy(string $keyPath, mixed $constantValue): ?ManagedObject
-    {
-        /** @var class-string<ManagedObject> $userClass */
-        $userClass = self::$userClass;
-        $fetchRequest = $userClass::fetchRequest();
-        $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath($keyPath), Expression::expressionForConstantValue($constantValue));
-        return $this->managedObjectContext->fetch($fetchRequest)->first?->serialized($this->serialization);
-    }
-
-    /**
-     * @throws Exception
-     */
     #[Action]
     public function login(): void
     {
         $this->isProtectedContentAvailable ?: throw new UnauthorizedException();
+        $date = new Date();
         /** @var ManagedObject $user */
         $user = $this->user;
-        $user->setValueForKey(bin2hex(read_random(32)), "token");
-        $this->managedObjectContext->save();
+        $username = $user->valueForKey("username");
+        $defaults = UserDefaults::standard();
+        $key = $defaults->string(JWTPrivateKey) ?? throw new InternalServerErrorException();
+        $validity = $defaults->integer(JWTValidityKey);
+        $encoder = new JWTEncoder($key);
+        $token = $encoder->encode([JWTIssuedField => $date->timeIntervalSinceReferenceDate, JWTUniqueIDField => base64_encode(random_bytes(16)), JWTIssuerField => $this->request->url->host, JWTNotBeforeField => $date->timeIntervalSinceReferenceDate, JWTExpirationField => $date->addingTimeInterval(60 * 60 * $validity)->timeIntervalSinceReferenceDate, "username" => $username]);
+        $user->setValueForKey($token, "token");
         $session = Application::shared()->session;
         $session->regenerateID();
-        $session->setValueForKey($user->valueForKey("username"), "user");
-        $this->content = json_encode($user, JSON_PRESERVE_ZERO_FRACTION);
+        $session->setValueForKey($username, "user");
+        $this->content = json_encode($user, JSON_PRESERVE_ZERO_FRACTION | JSON_THROW_ON_ERROR);
         $this->contentType = "application/json";
     }
 

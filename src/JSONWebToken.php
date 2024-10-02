d0c58456930bdead9b0491179cddb67ea03b9c21
dantesabatier <dantesabatier@me.com>
2024-10-02 13:55:32 -0600

    Update JSONWebToken.php

--- src/JSONWebToken.php
+++ src/JSONWebToken.php
@@ -13,7 +13,7 @@ class JSONWebToken implements JsonSerializable
     /** @var JSONWebTokenValues */
     public array $allValues;
 
-    public function __construct(public ?string $iss = null, public ?string $sub = null, public ?string $aud = null, public float $exp = 0, public float $nbf = 0, public float $iat = 0, public ?string $jti = null, public mixed $dat = null)
+    public function __construct(public ?string $iss = null, public ?string $sub = null, public ?string $aud = null, public float $exp = 0, public float $nbf = PHP_FLOAT_MAX, public float $iat = 0, public ?string $jti = null, public mixed $dat = null)
     {
         $this->allValues = ["iss" => $this->iss, "sub" => $this->sub, "aud" => $this->aud, "exp" => $this->exp, "nbf" => $this->nbf, "iat" => $this->iat, "jti" => $this->jti, "dat" => $this->dat];
     }

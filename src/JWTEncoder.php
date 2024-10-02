ed237d442289514db7bd57ed7881e5b14ece3abe
dantesabatier <dantesabatier@me.com>
2024-10-02 10:35:36 -0600

    Update JWTEncoder.php

--- src/JWTEncoder.php
+++ src/JWTEncoder.php
@@ -4,6 +4,9 @@ namespace Sabatier\Service;
 
 use Exception;
 
+/**
+ * @psalm-type JWT = array{iss: string|null, sub?: string, aud?: string, exp: float, nbf?: float, iat: float, jti: string, dat?: mixed|null}
+ */
 readonly class JWTEncoder
 {
     public function __construct(private string $key)
@@ -11,13 +14,13 @@ readonly class JWTEncoder
     }
 
     /**
-     * @param array<string, mixed> $value
+     * @param JWT $value
      * @return string
      * @throws Exception
      */
     public function encode(array $value): string
     {
-        $header = base64_encode(json_encode([JWTAlgorithmHeaderKey => "HS256", JWTTypeHeaderKey => "JWT"], JSON_THROW_ON_ERROR));
+        $header = base64_encode(json_encode(["alg" => "HS256", "typ" => "JWT"], JSON_THROW_ON_ERROR));
         $encoded = base64_encode(json_encode($value, JSON_THROW_ON_ERROR));
         $unsigned = sprintf("%s.%s", $header, $encoded);
         $signed = base64_encode(hash_hmac("sha256", $unsigned, $this->key, true));

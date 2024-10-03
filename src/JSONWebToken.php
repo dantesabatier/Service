bfa148a418d700619c5695fa268f589719efe060
dantesabatier <dantesabatier@me.com>
2024-10-03 09:06:28 -0600

    Update JSONWebToken.php

--- src/JSONWebToken.php
+++ src/JSONWebToken.php
@@ -4,18 +4,35 @@ namespace Sabatier\Service;
 
 use JsonSerializable;
 use Override;
+use Sabatier\Foundation\UndefinedKeyException;
 
 /**
- * @psalm-type JSONWebTokenValues = array{iss: string|null, sub: string|null, aud: string|null, exp: float, nbf: float, iat: float, jti: string|null, dat: mixed}
+ * @property-read string|null $iss
+ * @property-read string|null $sub
+ * @property-read string|null $aud
+ * @property-read float|null $exp
+ * @property-read float|null $nbf
+ * @property-read float|null $iat
+ * @property-read string|null $jti
+ * @property-read mixed $dat
+ * @psalm-type JSONWebTokenValues = array{iss: string|null, sub: string|null, aud: string|null, exp: float|null, nbf: float|null, iat: float|null, jti: string|null, dat: mixed}
  */
-class JSONWebToken implements JsonSerializable
+readonly class JSONWebToken implements JsonSerializable
 {
     /** @var JSONWebTokenValues */
-    readonly public array $allValues;
+    public array $allValues;
 
-    public function __construct(public ?string $iss = null, public ?string $sub = null, public ?string $aud = null, public float $exp = 0, public float $nbf = 0, public float $iat = 0, public ?string $jti = null, public mixed $dat = null)
+    public function __construct(?string $iss = null, ?string $sub = null, ?string $aud = null, ?float $exp = null, ?float $nbf = null, ?float $iat = null, ?string $jti = null, mixed $dat = null)
     {
-        $this->allValues = ["iss" => $this->iss, "sub" => $this->sub, "aud" => $this->aud, "exp" => $this->exp, "nbf" => $this->nbf, "iat" => $this->iat, "jti" => $this->jti, "dat" => $this->dat];
+        $this->allValues = ["iss" => $iss, "sub" => $sub, "aud" => $aud, "exp" => $exp, "nbf" => $nbf, "iat" => $iat, "jti" => $jti, "dat" => $dat];
+    }
+
+    public function __get(string $name)
+    {
+        $this->$name = match ($name) {
+            "iss", "sub", "aud", "exp", "nbf", "iat", "jti", "dat" => $this->allValues[$name] ?? null,
+            default => throw new UndefinedKeyException(),
+        };
     }
 
     /**
@@ -26,9 +43,6 @@ class JSONWebToken implements JsonSerializable
     {
         $token = new JSONWebToken();
         $token->allValues = $values;
-        foreach ($token->allValues as $key => $value) {
-            $token->$key = $value;
-        }
         return $token;
     }
 

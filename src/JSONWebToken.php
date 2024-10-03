714325dbcb03889ed71e4b77e7fa2b59587d658d
dantesabatier <dantesabatier@me.com>
2024-10-03 12:07:15 -0600

    Update JSONWebToken.php

--- src/JSONWebToken.php
+++ src/JSONWebToken.php
@@ -7,6 +7,8 @@ use Override;
 use Sabatier\Foundation\UndefinedKeyException;
 
 /**
+ * @psalm-type JSONWebTokenValues = array{iss: string|null, sub: string|null, aud: string|null, exp: float|null, nbf: float|null, iat: float|null, jti: string|null, sec: mixed}
+ *
  * @property-read string|null $iss
  * @property-read string|null $sub
  * @property-read string|null $aud
@@ -14,23 +16,24 @@ use Sabatier\Foundation\UndefinedKeyException;
  * @property-read float|null $nbf
  * @property-read float|null $iat
  * @property-read string|null $jti
- * @property-read mixed $dat
- * @psalm-type JSONWebTokenValues = array{iss: string|null, sub: string|null, aud: string|null, exp: float|null, nbf: float|null, iat: float|null, jti: string|null, dat: mixed}
+ * @property-read mixed $sec
+ * @property-read JSONWebTokenValues $allValues
  */
 class JSONWebToken implements JsonSerializable
 {
     /** @var JSONWebTokenValues */
     private array $allValues;
 
-    public function __construct(?string $iss = null, ?string $sub = null, ?string $aud = null, ?float $exp = null, ?float $nbf = null, ?float $iat = null, ?string $jti = null, mixed $dat = null)
+    public function __construct(?string $iss = null, ?string $sub = null, ?string $aud = null, ?float $exp = null, ?float $nbf = null, ?float $iat = null, ?string $jti = null, mixed $sec = null)
     {
-        $this->allValues = ["iss" => $iss, "sub" => $sub, "aud" => $aud, "exp" => $exp, "nbf" => $nbf, "iat" => $iat, "jti" => $jti, "dat" => $dat];
+        $this->allValues = ["iss" => $iss, "sub" => $sub, "aud" => $aud, "exp" => $exp, "nbf" => $nbf, "iat" => $iat, "jti" => $jti, "sec" => $sec];
     }
 
     public function __get(string $name)
     {
         return match ($name) {
-            "iss", "sub", "aud", "exp", "nbf", "iat", "jti", "dat" => $this->allValues[$name] ?? null,
+            "iss", "sub", "aud", "exp", "nbf", "iat", "jti", "sec" => $this->allValues[$name] ?? null,
+            "allValues" => $this->allValues,
             default => throw new UndefinedKeyException(),
         };
     }

40070a3db0d676fd88cacbb3db7b0bace1380a33
dantesabatier <dantesabatier@me.com>
2024-10-06 02:57:24 -0600

    Update ViewController.php

--- src/ViewController.php
+++ src/ViewController.php
@@ -99,6 +99,6 @@ abstract class ViewController extends Responder
         if ($this->request->httpMethod === HTTPRequestMethod::get) {
             $this->loadView();
         }
-        return parent::response();
+        return new HTTPURLResponse($this->request->url, $this->statusCode);
     }
 }

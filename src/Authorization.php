4553a199e6401a18b1c2cf918e27e1ebe535478e
dantesabatier <dantesabatier@me.com>
2024-10-07 21:47:56 -0600

    Update Authorization.php

--- src/Authorization.php
+++ src/Authorization.php
@@ -95,7 +95,7 @@ readonly class Authorization
     {
         $application = Application::shared();
         /** @var string|null $username */
-        $username = $this->credential?->user ?? $application->session->valueForKey("user");
+        $username = $this->credential?->user;
         if (!$username) {
             return null;
         }

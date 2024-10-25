2e0a5ce1584606e93b112b2609412284763d42c2
dantesabatier <dantesabatier@me.com>
2024-10-25 08:56:39 -0600

    Update Responder.php

--- src/Responder.php
+++ src/Responder.php
@@ -6,6 +6,10 @@ use Exception;
 use JetBrains\PhpStorm\ExpectedValues;
 use ReflectionClass;
 use ReflectionMethod;
+use Sabatier\CoreData\AttributeType;
+use Sabatier\CoreData\ExpressionDescription;
+use Sabatier\CoreData\FetchRequest;
+use Sabatier\CoreData\FetchRequestResultType;
 use Sabatier\CoreData\ManagedObjectContext;
 use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\CompareOptions;
@@ -15,7 +19,13 @@ use Sabatier\Foundation\Networking\HTTPStatusCode;
 use Sabatier\Foundation\Networking\HTTPURLResponse;
 use Sabatier\Foundation\Networking\URLRequest;
 use Sabatier\Foundation\ObjectClass;
+use Sabatier\Foundation\Predicates\ComparisonPredicate;
+use Sabatier\Foundation\Predicates\CompoundPredicate;
+use Sabatier\Foundation\Predicates\Expression;
+use Sabatier\Foundation\Predicates\Predicate;
+use Sabatier\Foundation\SortDescriptor;
 use Sabatier\Foundation\URLComponents;
+use Sabatier\Foundation\URLQueryItem;
 use function Sabatier\Foundation\string_is_equal;
 use function Sabatier\Foundation\url_validate;
 
@@ -26,7 +36,11 @@ use function Sabatier\Foundation\url_validate;
 abstract class Responder extends ObjectClass
 {
     public readonly URLRequest $request;
+    /** @var Dictionary<mixed>|null */
     public readonly ?Dictionary $serialization;
+    /** @var Dictionary<mixed> */
+    public Dictionary $headerFields;
+    public readonly FetchRequest $fetchRequest;
     public readonly ManagedObjectContext $managedObjectContext;
     /** @var ArrayClass<string> */
     public ArrayClass $allowedMethods;
@@ -34,8 +48,6 @@ abstract class Responder extends ObjectClass
     #[ExpectedValues(valuesFromClass: HTTPStatusCode::class)]
     public int $statusCode = HTTPStatusCode::ok;
     public ?string $content = null;
-    /** @var Dictionary<mixed> */
-    public Dictionary $headerFields;
     public readonly ?string $selector;
     public readonly bool $isEndpoint;
     public readonly bool $isActionable;
@@ -44,9 +56,10 @@ abstract class Responder extends ObjectClass
     {
         unset($this->request);
         unset($this->serialization);
+        unset($this->headerFields);
+        unset($this->fetchRequest);
         unset($this->managedObjectContext);
         unset($this->allowedMethods);
-        unset($this->headerFields);
         unset($this->selector);
         unset($this->isEndpoint);
         unset($this->isActionable);
@@ -60,9 +73,10 @@ abstract class Responder extends ObjectClass
         return $this->$name = match ($name) {
             "request" => Application::shared()->request,
             "serialization" => $this->serialization(),
+            "headerFields" => $this->headerFields(),
+            "fetchRequest" => $this->fetchRequest(),
             "managedObjectContext" => Application::shared()->persistentContainer->viewContext,
             "allowedMethods" => new ArrayClass([HTTPRequestMethod::head, HTTPRequestMethod::options, HTTPRequestMethod::get, HTTPRequestMethod::post, HTTPRequestMethod::patch, HTTPRequestMethod::put, HTTPRequestMethod::delete]),
-            "headerFields" => $this->headerFields(),
             "selector" => $this->selector(),
             "isEndpoint" => $this->isEndpoint(),
             "isActionable" => $this->selector !== null,
@@ -70,15 +84,99 @@ abstract class Responder extends ObjectClass
         };
     }
 
-    /**
-     * @throws Exception
-     */
     private function serialization(): ?Dictionary
     {
         if (!($string = $this->request->valueForHttpHeaderField("serialization")) || !json_validate($string) || !($array = json_decode($string, true))) {
             return null;
         }
         return Dictionary::dictionaryWithArray($array);
+    }
+
+    private function headerFields(): Dictionary
+    {
+        /** @var Dictionary<mixed> $headerFields */
+        $headerFields = new Dictionary();
+        if ($origin = $this->request->valueForHttpHeaderField("Origin")) {
+            $headerFields["Access-Control-Allow-Origin"] = $origin;
+            $headerFields["Access-Control-Allow-Credentials"] = true;
+            $headerFields["Vary"] = "Origin";
+        }
+        if ($value = $this->request->valueForHttpHeaderField("Access-Control-Request-Method")) {
+            $headerFields["Access-Control-Allow-Methods"] = $value;
+        }
+        if ($value = $this->request->valueForHttpHeaderField("Access-Control-Request-Headers")) {
+            $headerFields["Access-Control-Allow-Headers"] = $value;
+        }
+        return $headerFields;
+    }
+
+    private function fetchRequest(): FetchRequest
+    {
+        $fetchRequest = new FetchRequest();
+        $components = new URLComponents((string)$this->request->url);
+        if ($queryItems = $components->queryItems) {
+            if ($item = $queryItems->first(fn(URLQueryItem $item): bool => string_is_equal($item->name, "fetchRequest", CompareOptions::caseInsensitive))) {
+                if (($value = $item->value) && ($json = base64_decode($value)) && (json_validate($json))) {
+                    $decoded = json_decode($json);
+                    if (property_exists($decoded, "predicate")) {
+                        $predicate = $decoded->predicate;
+                        if (property_exists($predicate, "format")) {
+                            $fetchRequest->predicate = Predicate::format($predicate->format, ArrayClass::arrayWithArray($predicate->arguments ?? []));
+                        }
+                    }
+                    $fetchRequest->includesSubentities = $decoded->includesSubentities ?? true;
+                    $fetchRequest->fetchLimit = $decoded->fetchLimit ?? 0;
+                    $fetchRequest->fetchOffset = $decoded->fetchOffset ?? 0;
+                    $fetchRequest->fetchBatchSize = $decoded->fetchBatchSize ?? 0;
+                    if (property_exists($decoded, "sortDescriptors")) {
+                        /** @psalm-suppress InvalidPropertyAssignmentValue */
+                        $fetchRequest->sortDescriptors = (new ArrayClass($decoded->sortDescriptors))->compactMap(fn(object $obj): ?SortDescriptor => property_exists($obj, "key") ? new SortDescriptor($obj->key, $obj->ascending) : null);
+                    }
+                    if (property_exists($decoded, "resultType")) {
+                        $fetchRequest->resultType = FetchRequestResultType::from($decoded->resultType);
+                    }
+                    if (property_exists($decoded, "propertiesToFetch")) {
+                        /** @psalm-suppress InvalidPropertyAssignmentValue */
+                        $fetchRequest->propertiesToFetch = (new ArrayClass($decoded->propertiesToFetch))->compactMap(function (mixed $element): ExpressionDescription|string|null {
+                            if (is_string($element)) {
+                                return $element;
+                            }
+                            if (is_object($element) && (property_exists($element, "name") && property_exists($element, "expression"))) {
+                                $expression = $element->expression;
+                                if (property_exists($expression, "format")) {
+                                    $expressionDescription = new ExpressionDescription();
+                                    $expressionDescription->name = $element->name;
+                                    $expressionDescription->expression = Expression::expressionWithFormat($expression->format, ArrayClass::arrayWithArray($expression->arguments ?? []));
+                                    if (property_exists($element, "resultType")) {
+                                        $expressionDescription->resultType = AttributeType::from($element->resultType);
+                                    }
+                                    return $expressionDescription;
+                                }
+                            }
+                            return null;
+                        });
+                    }
+                    $fetchRequest->returnsDistinctResults = $decoded->returnsDistinctResults ?? false;
+                    if (property_exists($decoded, "propertiesToGroupBy")) {
+                        $fetchRequest->propertiesToGroupBy = new ArrayClass($decoded->propertiesToGroupBy);
+                    }
+                    if (property_exists($decoded, "havingPredicate")) {
+                        $havingPredicate = $decoded->havingPredicate;
+                        if (property_exists($havingPredicate, "format")) {
+                            $fetchRequest->havingPredicate = Predicate::format($havingPredicate->format, ArrayClass::arrayWithArray($havingPredicate->arguments ?? []));
+                        }
+                    }
+                }
+            } else {
+                $predicates = $queryItems->map(fn(URLQueryItem $item): ComparisonPredicate => new ComparisonPredicate(Expression::expressionForKeyPath($item->name), Expression::expressionForConstantValue($item->value)));
+                /** @psalm-suppress InvalidArgument */
+                $fetchRequest->predicate = $predicates->count > 1 ? CompoundPredicate::andPredicateWithSubpredicates($predicates) : $predicates->first;
+            }
+        }
+        if ($serialization = $this->serialization) {
+            $fetchRequest->serialization = $serialization;
+        }
+        return $fetchRequest;
     }
 
     private function isEndpoint(): bool
@@ -113,24 +211,6 @@ abstract class Responder extends ObjectClass
             }
         }
         return null;
-    }
-
-    private function headerFields(): Dictionary
-    {
-        /** @var Dictionary<mixed> $headerFields */
-        $headerFields = new Dictionary();
-        if ($origin = $this->request->valueForHttpHeaderField("Origin")) {
-            $headerFields["Access-Control-Allow-Origin"] = $origin;
-            $headerFields["Access-Control-Allow-Credentials"] = true;
-            $headerFields["Vary"] = "Origin";
-        }
-        if ($value = $this->request->valueForHttpHeaderField("Access-Control-Request-Method")) {
-            $headerFields["Access-Control-Allow-Methods"] = $value;
-        }
-        if ($value = $this->request->valueForHttpHeaderField("Access-Control-Request-Headers")) {
-            $headerFields["Access-Control-Allow-Headers"] = $value;
-        }
-        return $headerFields;
     }
 
     /**

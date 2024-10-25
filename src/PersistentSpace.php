8dd519dca2ca2daea572400e2d163c1ae14f1a85
dantesabatier <dantesabatier@me.com>
2024-10-25 08:56:43 -0600

    Update PersistentSpace.php

--- src/PersistentSpace.php
+++ src/PersistentSpace.php
@@ -7,10 +7,8 @@ namespace Sabatier\Service;
 use Exception;
 use Override;
 use Sabatier\CoreData\AtomicStore;
-use Sabatier\CoreData\AttributeType;
 use Sabatier\CoreData\BatchFaultingArray;
 use Sabatier\CoreData\EntityDescription;
-use Sabatier\CoreData\ExpressionDescription;
 use Sabatier\CoreData\FetchRequest;
 use Sabatier\CoreData\FetchRequestResultType;
 use Sabatier\CoreData\ManagedObject;
@@ -28,7 +26,6 @@ use Sabatier\Foundation\Predicates\CompoundPredicate;
 use Sabatier\Foundation\Predicates\Expression;
 use Sabatier\Foundation\Predicates\ExpressionType;
 use Sabatier\Foundation\Predicates\Predicate;
-use Sabatier\Foundation\SortDescriptor;
 use Sabatier\Foundation\URLComponents;
 use Sabatier\Foundation\URLQueryItem;
 use function Sabatier\Foundation\string_is_equal;
@@ -37,12 +34,10 @@ class PersistentSpace extends Responder
 {
     private readonly ?AtomicStore $atomicStore;
     private readonly EntityDescription $entity;
-    private readonly FetchRequest $fetchRequest;
 
     public function __construct()
     {
         parent::__construct();
-        unset($this->fetchRequest);
         unset($this->entity);
         unset($this->atomicStore);
     }
@@ -60,91 +55,6 @@ class PersistentSpace extends Responder
         }
         if ($name === "entity") {
             $this->$name = $this->managedObjectContext->persistentStoreCoordinator?->managedObjectModel?->entitiesByName?->valueForKey($this->request->url->lastPathComponent) ?? throw new NotFoundException("Unable to load entity \"{$this->request->url->lastPathComponent}\"");
-            return $this->$name;
-        }
-        if ($name === "fetchRequest") {
-            $fetchRequest = new FetchRequest();
-            $fetchRequest->entity = $this->entity;
-            $components = new URLComponents((string)$this->request->url);
-            if ($queryItems = $components->queryItems) {
-                if ($item = $queryItems->first(fn(URLQueryItem $item): bool => string_is_equal($item->name, "fetchRequest", CompareOptions::caseInsensitive))) {
-                    if (($value = $item->value) && ($json = base64_decode($value)) && ($decoded = json_decode($json, null, 512, JSON_THROW_ON_ERROR))) {
-                        if (property_exists($decoded, "predicate")) {
-                            $predicate = $decoded->predicate;
-                            if (property_exists($predicate, "format")) {
-                                $fetchRequest->predicate = Predicate::format($predicate->format, ArrayClass::arrayWithArray($predicate->arguments ?? []));
-                            }
-                        }
-                        $fetchRequest->includesSubentities = $decoded->includesSubentities ?? true;
-                        $fetchRequest->fetchLimit = $decoded->fetchLimit ?? 0;
-                        $fetchRequest->fetchOffset = $decoded->fetchOffset ?? 0;
-                        $fetchRequest->fetchBatchSize = $decoded->fetchBatchSize ?? 0;
-                        if (property_exists($decoded, "sortDescriptors")) {
-                            /** @psalm-suppress InvalidPropertyAssignmentValue */
-                            $fetchRequest->sortDescriptors = (new ArrayClass($decoded->sortDescriptors))->compactMap(fn(object $obj): ?SortDescriptor => property_exists($obj, "key") ? new SortDescriptor($obj->key, $obj->ascending) : null);
-                        }
-                        if (property_exists($decoded, "resultType")) {
-                            $fetchRequest->resultType = FetchRequestResultType::from($decoded->resultType);
-                        }
-                        if (property_exists($decoded, "propertiesToFetch")) {
-                            /** @psalm-suppress InvalidPropertyAssignmentValue */
-                            $fetchRequest->propertiesToFetch = (new ArrayClass($decoded->propertiesToFetch))->compactMap(function (mixed $element): ExpressionDescription|string|null {
-                                if (is_string($element)) {
-                                    return $element;
-                                }
-                                if (is_object($element) && (property_exists($element, "name") && property_exists($element, "expression"))) {
-                                    $expression = $element->expression;
-                                    if (property_exists($expression, "format")) {
-                                        $expressionDescription = new ExpressionDescription();
-                                        $expressionDescription->name = $element->name;
-                                        $expressionDescription->expression = Expression::expressionWithFormat($expression->format, ArrayClass::arrayWithArray($expression->arguments ?? []));
-                                        if (property_exists($element, "resultType")) {
-                                            $expressionDescription->resultType = AttributeType::from($element->resultType);
-                                        }
-                                        return $expressionDescription;
-                                    }
-                                }
-                                return null;
-                            });
-                        }
-                        $fetchRequest->returnsDistinctResults = $decoded->returnsDistinctResults ?? false;
-                        if (property_exists($decoded, "propertiesToGroupBy")) {
-                            $fetchRequest->propertiesToGroupBy = new ArrayClass($decoded->propertiesToGroupBy);
-                        }
-                        if (property_exists($decoded, "havingPredicate")) {
-                            $havingPredicate = $decoded->havingPredicate;
-                            if (property_exists($havingPredicate, "format")) {
-                                $fetchRequest->havingPredicate = Predicate::format($havingPredicate->format, ArrayClass::arrayWithArray($havingPredicate->arguments ?? []));
-                            }
-                        }
-                    }
-                } else {
-                    $predicates = $queryItems->map(fn(URLQueryItem $item): ComparisonPredicate => new ComparisonPredicate(Expression::expressionForKeyPath($item->name), Expression::expressionForConstantValue($item->value)));
-                    /** @psalm-suppress InvalidArgument */
-                    $fetchRequest->predicate = $predicates->count > 1 ? CompoundPredicate::andPredicateWithSubpredicates($predicates) : $predicates->first;
-                }
-            }
-            if (($predicate = $fetchRequest->predicate) && ($store = $this->atomicStore)) {
-                $fn = function (CompoundPredicate|ComparisonPredicate|Predicate $predicate) use ($store, &$fn): Predicate {
-                    if ($predicate instanceof ComparisonPredicate) {
-                        $expressions = new ArrayClass([$predicate->rightExpression, $predicate->leftExpression]);
-                        if (($keyPathExpression = $expressions->first(fn(Expression $e): bool => $e->expressionType === ExpressionType::keyPath && str_ends_with($e->keyPath(), SQLEntity::primaryKeyName))) && ($constantValueExpression = $expressions->first(fn(Expression $e): bool => !$e->isEqual($keyPathExpression)))) {
-                            $expressionForConstantValue = Expression::expressionForConstantValue($store->objectID($this->entity, $constantValueExpression->constantValue()));
-                            $rightExpression = $keyPathExpression === $predicate->rightExpression ? $keyPathExpression : $expressionForConstantValue;
-                            $leftExpression = $constantValueExpression === $predicate->leftExpression ? $expressionForConstantValue : $keyPathExpression;
-                            return new ComparisonPredicate($rightExpression, $leftExpression, $predicate->predicateOperatorType, $predicate->comparisonPredicateModifier, $predicate->options);
-                        }
-                        return $predicate;
-                    }
-                    /** @psalm-suppress all */
-                    return new CompoundPredicate($predicate->compoundPredicateType, $predicate->subpredicates->map(fn(CompoundPredicate|ComparisonPredicate $subpredicate): CompoundPredicate|ComparisonPredicate => $fn($subpredicate)));
-                };
-                $fetchRequest->predicate = $fn($predicate);
-            }
-            if ($serialization = $this->serialization) {
-                $fetchRequest->serialization = $serialization;
-            }
-            $this->$name = $fetchRequest;
             return $this->$name;
         }
         return parent::__get($name);
@@ -165,6 +75,24 @@ class PersistentSpace extends Responder
             case HTTPRequestMethod::get:
             case HTTPRequestMethod::head:
                 $fetchRequest = $this->fetchRequest;
+                $fetchRequest->entity = $this->entity;
+                if (($predicate = $fetchRequest->predicate) && ($store = $this->atomicStore)) {
+                    $fn = function (CompoundPredicate|ComparisonPredicate|Predicate $predicate) use ($store, &$fn): Predicate {
+                        if ($predicate instanceof ComparisonPredicate) {
+                            $expressions = new ArrayClass([$predicate->rightExpression, $predicate->leftExpression]);
+                            if (($keyPathExpression = $expressions->first(fn(Expression $e): bool => $e->expressionType === ExpressionType::keyPath && str_ends_with($e->keyPath(), SQLEntity::primaryKeyName))) && ($constantValueExpression = $expressions->first(fn(Expression $e): bool => !$e->isEqual($keyPathExpression)))) {
+                                $expressionForConstantValue = Expression::expressionForConstantValue($store->objectID($this->entity, $constantValueExpression->constantValue()));
+                                $rightExpression = $keyPathExpression === $predicate->rightExpression ? $keyPathExpression : $expressionForConstantValue;
+                                $leftExpression = $constantValueExpression === $predicate->leftExpression ? $expressionForConstantValue : $keyPathExpression;
+                                return new ComparisonPredicate($rightExpression, $leftExpression, $predicate->predicateOperatorType, $predicate->comparisonPredicateModifier, $predicate->options);
+                            }
+                            return $predicate;
+                        }
+                        /** @psalm-suppress all */
+                        return new CompoundPredicate($predicate->compoundPredicateType, $predicate->subpredicates->map(fn(CompoundPredicate|ComparisonPredicate $subpredicate): CompoundPredicate|ComparisonPredicate => $fn($subpredicate)));
+                    };
+                    $fetchRequest->predicate = $fn($predicate);
+                }
                 $fetchRequestResult = match ($fetchRequest->resultType) {
                     FetchRequestResultType::managedObjectResultType, FetchRequestResultType::managedObjectIDResultType,
                     FetchRequestResultType::dictionaryResultType => $context->fetch($fetchRequest),

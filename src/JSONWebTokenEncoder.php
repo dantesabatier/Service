ac8671dd436c220fd19f670b7d54c260b3890d8e
dantesabatier <dantesabatier@me.com>
2024-11-18 09:11:29 -0600

    Update JSONWebTokenEncoder.php

--- src/JSONWebTokenEncoder.php
+++ src/JSONWebTokenEncoder.php
@@ -3,16 +3,10 @@
 namespace Sabatier\Service;
 
 use Exception;
-use InvalidArgumentException;
 
-class JSONWebTokenEncoder
+readonly class JSONWebTokenEncoder
 {
-    public function __construct(private readonly string $key, private string $algorithm = "HS256" {
-        set {
-            hash_algos()[$value] ?: throw new InvalidArgumentException();
-            $this->algorithm = $value;
-        }
-    })
+    public function __construct(private string $key)
     {
     }
 
@@ -21,7 +15,7 @@ class JSONWebTokenEncoder
      */
     public function encode(JSONWebToken $token): string
     {
-        $header = base64_encode(json_encode(["alg" => $this->algorithm, "typ" => "JWT"], JSON_THROW_ON_ERROR));
+        $header = base64_encode(json_encode(["alg" => "HS256", "typ" => "JWT"], JSON_THROW_ON_ERROR));
         $encoded = base64_encode(json_encode($token, JSON_THROW_ON_ERROR));
         $unsigned = sprintf("%s.%s", $header, $encoded);
         $signed = base64_encode(hash_hmac("sha256", $unsigned, $this->key, true));

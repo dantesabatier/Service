0b208cdf38f12a1695229fe7b911d62c0ce9753e
dantesabatier <dantesabatier@me.com>
2024-11-25 14:10:53 -0600

    Update NativeEmitter.php

--- src/NativeEmitter.php
+++ src/NativeEmitter.php
@@ -3,12 +3,11 @@
 namespace Sabatier\Service;
 
 use Sabatier\Foundation\Dictionary;
-use Sabatier\Foundation\Networking\HTTPURLResponse;
 use function Sabatier\Foundation\human_readable_value;
 
 class NativeEmitter extends Emitter
 {
-    public function emit(HTTPURLResponse $response, Dictionary $headers, ?string $content = null): never
+    public function emit(Response $response, Dictionary $headers, ?string $content = null): never
     {
         if (headers_sent()) {
             die();
@@ -16,7 +15,7 @@ class NativeEmitter extends Emitter
         foreach (["Expires", "Cache-Control", "Pragma"] as $header) {
             header_remove($header);
         }
-        header(sprintf("%s %s %s", $response->httpVersion, $response->statusCode, HTTPURLResponse::localizedString($response->statusCode)));
+        header(sprintf("%s %s %s", $response->httpVersion, $response->statusCode, Response::localizedString($response->statusCode)));
         foreach ($headers as $key => $value) {
             header(sprintf("%s: %s", $key, human_readable_value($value)));
         }

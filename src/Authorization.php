0494f166267278399148f152a55be4136fc0ff8a
dantesabatier <dantesabatier@me.com>
2024-11-27 15:50:50 -0600

    Authorization

--- src/Authorization.php
+++ src/Authorization.php
@@ -2,7 +2,11 @@
 
 namespace Sabatier\Service;
 
+use Sabatier\CoreData\EntityDescription;
+use Sabatier\CoreData\FetchRequest;
 use Sabatier\Foundation\Networking\URLCredential;
+use Sabatier\Foundation\Predicates\ComparisonPredicate;
+use Sabatier\Foundation\Predicates\Expression;
 
 abstract class Authorization
 {
@@ -12,12 +16,17 @@ abstract class Authorization
     abstract public bool $isValid {
         get;
     }
-    public ?Authenticatable $user {
+    public Authorization $user {
         get {
             if (!($username = $this->credential?->user)) {
                 return null;
             }
-            return new IdentityManager($username, Application::shared()->request->serialization)->user;
+            $context = Application::shared()->persistentContainer->viewContext;
+            /** @var FetchRequest<Authenticatable> $fetchRequest */
+            $fetchRequest = new FetchRequest();
+            $fetchRequest->entity = EntityDescription::entity("User", $context);
+            $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($username));
+            return $context->fetch($fetchRequest)->first?->serialized(Application::shared()->request->serialization);
         }
     }
 

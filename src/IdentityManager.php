7e1bb63d08c4650cd80714b97e9bdfb5ac7dffc7
dantesabatier <dantesabatier@me.com>
2024-11-27 14:22:38 -0600

    Create IdentityManager.php

---
+++ src/IdentityManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sabatier\Service;
+
+use Sabatier\CoreData\EntityDescription;
+use Sabatier\CoreData\FetchRequest;
+use Sabatier\Foundation\Dictionary;
+use Sabatier\Foundation\Predicates\ComparisonPredicate;
+use Sabatier\Foundation\Predicates\Expression;
+
+class IdentityManager
+{
+    public ?Authenticatable $user {
+        get {
+            $context = Application::shared()->persistentContainer->viewContext;
+            $fetchRequest = new FetchRequest();
+            $fetchRequest->entity = EntityDescription::entity("User", $context);
+            $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($this->username));
+            return $context->fetch($fetchRequest)->first?->serialized($this->serialization);
+        }
+    }
+
+    public function __construct(public readonly string $username, public readonly ?Dictionary $serialization = null)
+    {
+    }
+}

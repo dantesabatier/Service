d71a110fce8b757767028cab2f7cd11914ccc2de
dantesabatier <dantesabatier@me.com>
2024-11-27 12:11:49 -0600

    Update PersistentSpace.php

--- src/PersistentSpace.php
+++ src/PersistentSpace.php
@@ -10,7 +10,7 @@ use Sabatier\Foundation\Networking\HTTPRequestMethod;
 class PersistentSpace extends Responder
 {
     public EntityDescription $entity {
-        get => $this->entity ??= $this->managedObjectContext->persistentStoreCoordinator?->managedObjectModel?->entitiesByName?->valueForKey($this->request->url->lastPathComponent) ?? throw new NotFoundException("Unable to load entity \"{$this->request->url->lastPathComponent}\"");
+        get => $this->entity ??= $this->managedObjectContext->persistentStoreCoordinator?->managedObjectModel?->entitiesByName?->valueForKey($this->request->url->lastPathComponent) ?? throw new NotFoundException("The requested entity \"{$this->request->url->lastPathComponent}\" does not exist.");
     }
     public bool $isFirstResponder {
         get => $this->managedObjectContext->persistentStoreCoordinator?->managedObjectModel?->entitiesByName?->offsetExists($this->request->url->lastPathComponent) ?? false;

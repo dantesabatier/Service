6fd09f781e823f74606836e54399d2706518df32
dantesabatier <dantesabatier@me.com>
2024-11-28 14:33:09 -0600

    Update DigestAuthorization.php

--- src/DigestAuthorization.php
+++ src/DigestAuthorization.php
@@ -36,8 +36,8 @@ class DigestAuthorization extends Authorization
                 "SHA-256" => "sha256",
                 default => "md5"
             };
-            $HA1 = hash($algo, "$username:$this->host:$password");
-            $HA2 = hash($algo, "$this->method:$uri");
+            $HA1 = hash($algo, "$username:{$this->request->url->host}:$password");
+            $HA2 = hash($algo, "{$this->request->httpMethod}:$uri");
             $response = hash($algo, "$HA1:$nonce:$nc:$cnonce:$qop:$HA2");
             return $parameters["response"] === $response;
         }

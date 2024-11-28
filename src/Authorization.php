6b43e6e1a322f2cd9b17ff934a9e5a57e7c46211
dantesabatier <dantesabatier@me.com>
2024-11-28 14:13:22 -0600

    Update Authorization.php

--- src/Authorization.php
+++ src/Authorization.php
@@ -3,6 +3,7 @@
 namespace Sabatier\Service;
 
 use JetBrains\PhpStorm\ExpectedValues;
+use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\Networking\URLCredential;
 
@@ -19,11 +20,11 @@ abstract class Authorization
             if (!($username = $this->credential?->user)) {
                 return null;
             }
-            return new IdentityManager(Application::shared()->persistentContainer->viewContext, $username, Application::shared()->request->serialization)->currenUser;
+            return new IdentityManager(Application::shared()->persistentContainer->viewContext, $username, $this->serialization)->currenUser;
         }
     }
 
-    public function __construct(public readonly string $credentials, #[ExpectedValues(valuesFromClass: HTTPRequestMethod::class)] public string $method = HTTPRequestMethod::get, public readonly ?string $host = null, public readonly ?int $port = null)
+    public function __construct(public readonly string $credentials, #[ExpectedValues(valuesFromClass: HTTPRequestMethod::class)] public string $method = HTTPRequestMethod::get, public readonly ?string $host = null, public readonly ?Dictionary $serialization = null)
     {
     }
 }

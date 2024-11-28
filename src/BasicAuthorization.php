c8816608569e80a715cb96f846ac162cec682339
dantesabatier <dantesabatier@me.com>
2024-11-28 14:25:04 -0600

    Update BasicAuthorization.php

--- src/BasicAuthorization.php
+++ src/BasicAuthorization.php
@@ -9,7 +9,7 @@ class BasicAuthorization extends Authorization
 {
     public ?URLCredential $credential {
         get {
-            $components = explode(":", base64_decode($this->credentials));
+            $components = explode(":", base64_decode($this->data));
             if (count($components) !== 2) {
                 return null;
             }

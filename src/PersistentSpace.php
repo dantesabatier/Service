11839c561665146d8ad498a634ceec5794550ab9
dantesabatier <dantesabatier@me.com>
2024-11-30 22:27:03 -0600

    Update PersistentSpace.php

--- src/PersistentSpace.php
+++ src/PersistentSpace.php
@@ -21,7 +21,7 @@ class PersistentSpace extends Responder
             HTTPRequestMethod::post => new PersistentSpaceRespondentCreator($this),
             HTTPRequestMethod::patch => new PersistentSpaceRespondentUpdater($this),
             HTTPRequestMethod::delete => new PersistentSpaceRespondentDeleter($this),
-            HTTPRequestMethod::options => new Response($this),
+            HTTPRequestMethod::options => new PersistentSpaceRespondent($this),
             default => throw new MethodNotAllowedException()
         };
     }

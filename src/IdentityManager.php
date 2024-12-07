ac75338e082be380d3f7f05cbc5c56a90ab8d195
dantesabatier <dantesabatier@me.com>
2024-12-07 10:00:44 -0600

    Update IdentityManager.php

--- src/IdentityManager.php
+++ src/IdentityManager.php
@@ -5,8 +5,10 @@ namespace Sabatier\Service;
 use Sabatier\CoreData\EntityDescription;
 use Sabatier\CoreData\FetchRequest;
 use Sabatier\CoreData\ManagedObjectContext;
+use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\Predicates\ComparisonPredicate;
+use Sabatier\Foundation\Predicates\CompoundPredicate;
 use Sabatier\Foundation\Predicates\Expression;
 
 class IdentityManager
@@ -17,8 +19,11 @@ class IdentityManager
             /** @var FetchRequest<Authenticatable> $fetchRequest */
             $fetchRequest = new FetchRequest();
             $fetchRequest->entity = EntityDescription::entity("User", $context);
-            $fetchRequest->predicate = new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($this->username));
-            return $context->fetch($fetchRequest)->first?->serialized($this->serialization);
+            $fetchRequest->predicate = CompoundPredicate::andPredicateWithSubpredicates(new ArrayClass([new ComparisonPredicate(Expression::expressionForKeyPath("username"), Expression::expressionForConstantValue($this->username)), new ComparisonPredicate(Expression::expressionForKeyPath("isEnabled"), Expression::expressionForConstantValue(true))]));
+            if ($serialization = $this->serialization) {
+                $fetchRequest->serialization = $serialization;
+            }
+            return $context->fetch($fetchRequest)->first;
         }
     }
 

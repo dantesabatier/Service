5fb733301af015408dbe1a492425e2fc79725993
dantesabatier <dantesabatier@me.com>
2024-12-07 10:14:09 -0600

    Update Authorizable.php

--- src/Authorizable.php
+++ src/Authorizable.php
@@ -4,9 +4,6 @@ namespace Sabatier\Service;
 
 interface Authorizable extends Authenticatable
 {
-    public bool $isEnabled {
-        get;
-    }
     public ?Authorization $authorization {
         get;
     }

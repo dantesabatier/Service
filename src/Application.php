ba717de67876fcd2c7487ac1c3e284aef9b807b9
dantesabatier <dantesabatier@me.com>
2025-01-23 11:09:28 -0600

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -181,7 +181,10 @@ class Application extends Responder
             $session->start();
             $accessManager = $this->accessManager;
             if (!$responder->isProtectedContentAvailable && !$accessManager->isProtectedContentAvailable) {
-                throw new UnauthorizedException();
+                throw new UnauthorizedException(match ($this->request->httpMethod) {
+                    HTTPRequestMethod::get => "You do not have access to this resource.",
+                    default => "You do not have permission to perform this action."
+                });
             }
             $viewContext->transactionAuthor = match ($this->request->httpMethod) {
                 HTTPRequestMethod::post, HTTPRequestMethod::put, HTTPRequestMethod::patch, HTTPRequestMethod::delete => $accessManager->authentication->user?->username,

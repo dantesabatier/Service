82d33691f7a1486310d20abf5850eaac6e93e471
dantesabatier <dantesabatier@me.com>
2025-05-02 12:07:25 -0600

    Update Endpoint.php

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -7,6 +7,11 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS)]
 final readonly class Endpoint
 {
+    /**
+     * Constructor method for initializing the class with an optional path.
+     *
+     * @param string|null $path The file path or null if no path is provided. If not provided, the class name will be used as the path.
+     */
     public function __construct(public ?string $path = null)
     {
     }

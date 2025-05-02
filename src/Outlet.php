abc4d6be9b967457cd815b8f723cd1e880e6f7d4
dantesabatier <dantesabatier@me.com>
2025-05-02 12:16:41 -0600

    Update Outlet.php

--- src/Outlet.php
+++ src/Outlet.php
@@ -4,6 +4,9 @@ namespace Sabatier\Service;
 
 use Attribute;
 
+/**
+ * Attribute used to mark a property as visible to a view's renderer.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 final class Outlet
 {

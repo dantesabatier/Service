9a664aeaf8c930c92071014d58262e671aeaec0a
dantesabatier <dantesabatier@me.com>
2025-05-02 12:09:49 -0600

    Update Action.php

--- src/Action.php
+++ src/Action.php
@@ -6,6 +6,9 @@ use Attribute;
 use JetBrains\PhpStorm\ExpectedValues;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 
+/**
+ * Represents an attribute that can be used to annotate methods with an HTTP request method and an optional path.
+ */
 #[Attribute(Attribute::TARGET_METHOD)]
 final readonly class Action
 {

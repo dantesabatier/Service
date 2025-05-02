b8b163230c7192c0c2c11b3a759fb1337c6aef05
dantesabatier <dantesabatier@me.com>
2025-05-02 12:13:05 -0600

    Update Renderer.php

--- src/Renderer.php
+++ src/Renderer.php
@@ -4,12 +4,23 @@ namespace Sabatier\Service;
 
 use Sabatier\Foundation\Bundle;
 
+/**
+ * The Renderer class is responsible for rendering views using a provided Bundle.
+ */
 class Renderer
 {
     public function __construct(public Bundle $bundle)
     {
     }
 
+    /**
+     * Renders a view by its name and provided context.
+     *
+     * @param string $name The name of the view to be rendered.
+     * @param object|array $context The context data to be extracted and passed to the view.
+     * @return string The rendered output of the view as a string.
+     * @throws NotFoundException If the specified view does not exist.
+     */
     public function render(string $name, object|array $context): string
     {
         $path = $this->bundle->url($name, "php")?->path ?? throw new NotFoundException("The view named \"$name\" does not exists");

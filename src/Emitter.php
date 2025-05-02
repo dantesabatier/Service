933f239688019be1526b00840cde08d8e4c61d11
dantesabatier <dantesabatier@me.com>
2025-05-02 11:59:00 -0600

    Update Emitter.php

--- src/Emitter.php
+++ src/Emitter.php
@@ -5,8 +5,19 @@ namespace Sabatier\Service;
 use Sabatier\Foundation\Dictionary;
 use function Sabatier\Foundation\human_readable_value;
 
+/**
+ * Handles emitting an HTTP response to the client.
+ */
 class Emitter
 {
+    /**
+     * Emits the response to the client by sending headers and content and then terminates execution.
+     *
+     * @param Response $response The response object.
+     * @param Dictionary $headers An associative array of additional headers to send.
+     * @param string|null $content The body content to send in the response can be null.
+     * @return never This method does not return.
+     */
     public function emit(Response $response, Dictionary $headers, ?string $content = null): never
     {
         if (headers_sent()) {

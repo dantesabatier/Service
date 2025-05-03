be462dda168571581fa0260481c8763de006911f
dantesabatier <dantesabatier@me.com>
2025-05-02 18:39:39 -0600

    Update JSONWebTokenHS256EncoderStrategy.php

--- src/JSONWebTokenHS256EncoderStrategy.php
+++ src/JSONWebTokenHS256EncoderStrategy.php
@@ -10,7 +10,7 @@ class JSONWebTokenHS256EncoderStrategy extends JSONWebTokenEncoderStrategy
     #[Override]
     public function encode(JSONWebToken $token): string
     {
-        $header = base64_encode(json_encode(["alg" => "HS256", "typ" => "JWT"], JSON_THROW_ON_ERROR));
+        $header = base64_encode(json_encode(["alg" => JSONWebTokenSigningAlgorithm::hs256->value, "typ" => "JWT"], JSON_THROW_ON_ERROR));
         $encoded = base64_encode(json_encode($token, JSON_THROW_ON_ERROR));
         $unsigned = sprintf("%s.%s", $header, $encoded);
         $signed = base64_encode(hash_hmac("sha256", $unsigned, $this->key, true));

c0d94b7b115d0463a5a638ad826b51b195c6e419
dantesabatier <dantesabatier@me.com>
2025-05-24 22:58:23 -0600

    Update PersistentSpaceRespondentReader.php

--- src/PersistentSpaceRespondentReader.php
+++ src/PersistentSpaceRespondentReader.php
@@ -34,11 +34,11 @@ class PersistentSpaceRespondentReader extends PersistentSpaceRespondent
             if ($queryItems = $components->queryItems) {
                 if ($item = $queryItems->first(fn(URLQueryItem $item): bool => string_is_equal($item->name, "fetchRequest", CompareOptions::caseInsensitive))) {
                     if (($value = $item->value) && ($json = base64_decode($value)) && (json_validate($json))) {
-                        /** @var object{predicate: object{format: string, arguments?: array}, includesSubentities?: bool, fetchLimit?: int, fetchOffset?: int, fetchBatchSize?: int, sortDescriptors?: array, resultType: int, propertiesToFetch?: array, returnsDistinctResults?: bool, propertiesToGroupBy?: array, havingPredicate: object{format: string, arguments?: array}} $decoded */
+                        /** @var object{predicate: object{format: string, arguments?: array}, includesSubentities?: bool, fetchLimit?: int, fetchOffset?: int, fetchBatchSize?: int, sortDescriptors?: object{key: string, ascending: bool}[], resultType: int, propertiesToFetch?: array, returnsDistinctResults?: bool, propertiesToGroupBy?: array, havingPredicate: object{format: string, arguments?: array}} $decoded */
                         $decoded = json_decode($json);
-                        if (property_exists($decoded, "predicate")) {
+                        if (isset($decoded->predicate)) {
                             $predicate = $decoded->predicate;
-                            if (property_exists($predicate, "format")) {
+                            if (isset($predicate->format)) {
                                 $fetchRequest->predicate = Predicate::format($predicate->format, ArrayClass::arrayWithArray($predicate->arguments ?? []));
                             }
                         }
@@ -46,48 +46,62 @@ class PersistentSpaceRespondentReader extends PersistentSpaceRespondent
                         $fetchRequest->fetchLimit = $decoded->fetchLimit ?? 0;
                         $fetchRequest->fetchOffset = $decoded->fetchOffset ?? 0;
                         $fetchRequest->fetchBatchSize = $decoded->fetchBatchSize ?? 0;
-                        if (property_exists($decoded, "sortDescriptors")) {
-                            /** @psalm-suppress InvalidPropertyAssignmentValue */
-                            $fetchRequest->sortDescriptors = new ArrayClass($decoded->sortDescriptors)->compactMap(fn(object $obj): ?SortDescriptor => property_exists($obj, "key") ? new SortDescriptor($obj->key, $obj->ascending) : null);
+                        if (isset($decoded->sortDescriptors)) {
+                            $fetchRequest->sortDescriptors = new ArrayClass($decoded->sortDescriptors)->compactMap(
+                            /**
+                             * @param object{key: string, ascending?: bool} $obj
+                             * @return SortDescriptor|null
+                             */
+                                fn(object $obj): ?SortDescriptor => isset($obj->key) ? new SortDescriptor($obj->key, $obj->ascending ?? true) : null);
                         }
-                        if (property_exists($decoded, "resultType")) {
+                        if (isset($decoded->resultType)) {
                             $fetchRequest->resultType = FetchRequestResultType::from($decoded->resultType);
                         }
-                        if (property_exists($decoded, "propertiesToFetch")) {
-                            /** @psalm-suppress InvalidPropertyAssignmentValue */
-                            $fetchRequest->propertiesToFetch = new ArrayClass($decoded->propertiesToFetch)->compactMap(function (mixed $element): ExpressionDescription|string|null {
-                                if (is_string($element)) {
-                                    return $element;
-                                }
-                                if (is_object($element) && (property_exists($element, "name") && property_exists($element, "expression"))) {
-                                    $expression = $element->expression;
-                                    if (property_exists($expression, "format")) {
-                                        $expressionDescription = new ExpressionDescription();
-                                        $expressionDescription->name = $element->name;
-                                        $expressionDescription->expression = Expression::expressionWithFormat($expression->format, ArrayClass::arrayWithArray($expression->arguments ?? []));
-                                        if (property_exists($element, "resultType")) {
-                                            $expressionDescription->resultType = AttributeType::from($element->resultType);
-                                        }
-                                        return $expressionDescription;
+                        if (isset($decoded->propertiesToFetch)) {
+                            $fetchRequest->propertiesToFetch = new ArrayClass($decoded->propertiesToFetch)->compactMap(
+                            /**
+                             * @param string|object{name: string, expression: object{format: string}, resultType: int} $element
+                             * @return ExpressionDescription|string|null
+                             */
+                                function (mixed $element): ExpressionDescription|string|null {
+                                    if (is_string($element)) {
+                                        return $element;
                                     }
-                                }
-                                return null;
-                            });
+                                    if (!is_object($element)) {
+                                        return null;
+                                    }
+                                    if (!isset($element->name)) {
+                                        return null;
+                                    }
+                                    if (!isset($element->expression)) {
+                                        return null;
+                                    }
+                                    $expression = $element->expression;
+                                    if (!isset($expression->format)) {
+                                        return null;
+                                    }
+                                    $expressionDescription = new ExpressionDescription();
+                                    $expressionDescription->name = $element->name;
+                                    $expressionDescription->expression = Expression::expressionWithFormat($expression->format, ArrayClass::arrayWithArray($expression->arguments ?? []));
+                                    if (isset($element->resultType)) {
+                                        $expressionDescription->resultType = AttributeType::from($element->resultType);
+                                    }
+                                    return $expressionDescription;
+                                });
                         }
                         $fetchRequest->returnsDistinctResults = $decoded->returnsDistinctResults ?? false;
-                        if (property_exists($decoded, "propertiesToGroupBy")) {
+                        if (isset($decoded->propertiesToGroupBy)) {
                             $fetchRequest->propertiesToGroupBy = new ArrayClass($decoded->propertiesToGroupBy);
                         }
-                        if (property_exists($decoded, "havingPredicate")) {
+                        if (isset($decoded->havingPredicate)) {
                             $havingPredicate = $decoded->havingPredicate;
-                            if (property_exists($havingPredicate, "format")) {
+                            if (isset($havingPredicate->format)) {
                                 $fetchRequest->havingPredicate = Predicate::format($havingPredicate->format, ArrayClass::arrayWithArray($havingPredicate->arguments ?? []));
                             }
                         }
                     }
                 } else {
                     $predicates = $queryItems->map(fn(URLQueryItem $item): ComparisonPredicate => new ComparisonPredicate(Expression::expressionForKeyPath($item->name), Expression::expressionForConstantValue($item->value)));
-                    /** @psalm-suppress InvalidArgument */
                     $fetchRequest->predicate = $predicates->count > 1 ? CompoundPredicate::andPredicateWithSubpredicates($predicates) : $predicates->first;
                 }
             }

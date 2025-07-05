cb0297ab09b1bcbb486c2ddda02f8a815392b281
dantesabatier <dantesabatier@me.com>
2025-07-05 06:02:53 -0600

    Update BearerAuthentication.php

--- src/BearerAuthentication.php
+++ src/BearerAuthentication.php
@@ -2,7 +2,6 @@
 
 namespace Sabatier\Service;
 
-use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Networking\URLCredential;
 use Sabatier\Foundation\UserDefaults;
 
@@ -15,7 +14,7 @@ class BearerAuthentication extends Authentication
     private(set) ?URLCredential $credential {
         get {
             if (!isset($this->credential)) {
-                if (($key = UserDefaults::standard()->string(JWTPrivateKeyPreferenceKey)) && ($strategyClass = JSONWebTokenCoderStrategyFactory::getStrategyClass(JSONWebTokenCoderStrategyFactory::getDecoderStrategies() ?? new ArrayClass(), JSONWebTokenSigningAlgorithm::tryFrom((string)UserDefaults::standard()->string(JWTSignatureAlgorithmKey)) ?? JSONWebTokenSigningAlgorithm::hs256)) && ($username = new JSONWebTokenDecoder(new $strategyClass($key, $this->request->url->host))->decode($this->request->authParameter->value)->username)) {
+                if (($key = UserDefaults::standard()->string(JWTPrivateKeyPreferenceKey)) && ($JSONWebTokenDecoderStrategyClass = JSONWebTokenCoderStrategyFactory::shared()->getStrategyClass(JSONWebTokenCoderStrategyFactory::shared()->decoderStrategies, JSONWebTokenSigningAlgorithm::tryFrom((string)UserDefaults::standard()->string(JWTSignatureAlgorithmKey)) ?? JSONWebTokenSigningAlgorithm::hs256)) && ($username = new JSONWebTokenDecoder(new $JSONWebTokenDecoderStrategyClass($key, $this->request->url->host))->decode($this->request->authParameter->value)->username)) {
                     $this->credential = new URLCredential($username);
                 }
                 $this->credential ??= null;

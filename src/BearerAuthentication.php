846280b098c270207222baa2a32e6e885f687491
dantesabatier <dantesabatier@me.com>
2025-07-05 05:14:07 -0600

    Update BearerAuthentication.php

--- src/BearerAuthentication.php
+++ src/BearerAuthentication.php
@@ -15,7 +15,7 @@ class BearerAuthentication extends Authentication
     private(set) ?URLCredential $credential {
         get {
             if (!isset($this->credential)) {
-                if (($key = UserDefaults::standard()->string(JWTPrivateKeyPreferenceKey)) && ($strategyClass = JSONWebTokenStrategyFactory::getStrategyClass(JSONWebTokenStrategyFactory::getDecoderStrategies() ?? new ArrayClass(), JSONWebTokenSigningAlgorithm::tryFrom((string)UserDefaults::standard()->string(JWTSignatureAlgorithmKey)) ?? JSONWebTokenSigningAlgorithm::hs256)) && ($username = new JSONWebTokenDecoder(new $strategyClass($key, $this->request->url->host))->decode($this->request->authParameter->value)->username)) {
+                if (($key = UserDefaults::standard()->string(JWTPrivateKeyPreferenceKey)) && ($strategyClass = JSONWebTokenCoderStrategyFactory::getStrategyClass(JSONWebTokenCoderStrategyFactory::getDecoderStrategies() ?? new ArrayClass(), JSONWebTokenSigningAlgorithm::tryFrom((string)UserDefaults::standard()->string(JWTSignatureAlgorithmKey)) ?? JSONWebTokenSigningAlgorithm::hs256)) && ($username = new JSONWebTokenDecoder(new $strategyClass($key, $this->request->url->host))->decode($this->request->authParameter->value)->username)) {
                     $this->credential = new URLCredential($username);
                 }
                 $this->credential ??= null;

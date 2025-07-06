df9097aad085e829d20e16d2913d1bd2a66ee382
dantesabatier <dantesabatier@me.com>
2025-07-05 22:56:56 -0600

    Update JSONWebTokenException.php

--- src/JSONWebTokenException.php
+++ src/JSONWebTokenException.php
@@ -4,6 +4,7 @@ namespace Sabatier\Service;
 
 /**
  * Represents an exception specific to JSON Web Token (JWT) handling.
+ *
  * Typically, thrown when an error related to JWT creation, decoding, or validation occurs.
  */
 class JSONWebTokenException extends UnauthorizedException

5640321ddfbea1dea7ae9a24c7076054deaa12fe
dantesabatier <dantesabatier@me.com>
2025-07-06 01:42:12 -0600

    Create JSONWebTokenRS256EncoderStrategy.php

---
+++ src/JSONWebTokenRS256EncoderStrategy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Sabatier\Service;
+
+use Override;
+
+/**
+ * @phpstan-import-type JSONWebTokenValues from JSONWebToken
+ * @internal
+ */
+class JSONWebTokenRS256EncoderStrategy extends JSONWebTokenEncoderStrategy
+{
+    public static JSONWebTokenSigningAlgorithm $algorithm = JSONWebTokenSigningAlgorithm::rs256;
+
+    #[Override]
+    public function encode(JSONWebToken $token): string
+    {
+        $header = base64_encode(json_encode(["alg" => self::$algorithm->value, "typ" => "JWT"], JSON_THROW_ON_ERROR));
+        $encoded = base64_encode(json_encode($token, JSON_THROW_ON_ERROR));
+        $unsigned = "$header.$encoded";
+        if (!openssl_sign($unsigned, $signature, $this->key, OPENSSL_ALGO_SHA256)) {
+            throw new JSONWebTokenException("Unable to sign JWT with RS256.");
+        }
+        $signature = base64_encode($signature);
+        return "$header.$encoded.$signature";
+    }
+}

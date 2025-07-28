7237f81bcec80454b9e9dc137103da17fb1381e0
dantesabatier <dantesabatier@me.com>
2025-07-28 10:19:45 -0600

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -3,26 +3,18 @@
 namespace Sabatier\Service;
 
 use Exception;
-use ReflectionClass;
 use Sabatier\CoreData\PersistentContainer;
 use Sabatier\CoreData\PersistentStoreDescription;
-use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\Bundle;
-use Sabatier\Foundation\CompareOptions;
-use Sabatier\Foundation\DirectoryEnumerationOptions;
 use Sabatier\Foundation\Error;
-use Sabatier\Foundation\FileManager;
-use Sabatier\Foundation\FlattenSequence;
 use Sabatier\Foundation\InternalInconsistencyException;
 use Sabatier\Foundation\Networking\HTTPRequestMethod;
 use Sabatier\Foundation\Notification;
 use Sabatier\Foundation\NotificationCenter;
 use Sabatier\Foundation\ObjectClass;
 use Sabatier\Foundation\ProcessInfo;
-use Sabatier\Foundation\URL;
 use Sabatier\Foundation\UserDefaults;
 use Throwable;
-use function Sabatier\Foundation\string_is_equal;
 use const Sabatier\CoreData\PersistentHistoryTrackingKey;
 use const Sabatier\CoreData\PersistentStoreRemoteChange;
 use const Sabatier\CoreData\PersistentStoreRemoteChangeNotificationPostOptionKey;
@@ -119,132 +111,9 @@ class Application extends Responder
     {
     }
 
-    /**
-     * @param string $namespaceName
-     * @param URL $directoryURL
-     * @param URL $fileURL
-     * @return class-string<Responder>
-     */
-    private function buildClassName(string $namespaceName, URL $directoryURL, URL $fileURL): string
-    {
-        $directoryComponent = $directoryURL->lastPathComponent;
-        $fileNameWithoutExtension = FileManager::default()->displayName($fileURL->path);
-        /** @var class-string<Responder> */
-        return "$namespaceName\\$directoryComponent\\$fileNameWithoutExtension";
-    }
-
-    private function isValidResponderClass(string $className): bool
-    {
-        if (!class_exists($className) || !is_subclass_of($className, Responder::class)) {
-            return false;
-        }
-        $reflectionClass = new ReflectionClass($className);
-        return $reflectionClass->isInstantiable();
-    }
-
-    /**
-     * @param URL $directoryURL
-     * @return ArrayClass<URL>
-     */
-    private function filteredFileURLs(URL $directoryURL): ArrayClass
-    {
-        return FileManager::default()->contentsOfDirectory($directoryURL, null, DirectoryEnumerationOptions::skipsHiddenFiles)->filter(fn(URL $url): bool => string_is_equal($url->pathExtension, "php", CompareOptions::caseInsensitive));
-    }
-
-    /**
-     * @param string $namespaceName
-     * @param URL $directoryURL
-     * @return ArrayClass<class-string<Responder>>|null
-     */
-    private function responderClassesFromDirectory(string $namespaceName, URL $directoryURL): ?ArrayClass
-    {
-        if (!FileManager::default()->fileExists($directoryURL->path)) {
-            return null;
-        }
-        return $this->filteredFileURLs($directoryURL)->map(fn(URL $fileURL): string => $this->buildClassName($namespaceName, $directoryURL, $fileURL))->filter(fn(string $className): bool => $this->isValidResponderClass($className));
-    }
-
-
-    /**
-     * @param string $namespaceName
-     * @return FlattenSequence<class-string<Responder>>
-     */
-    private function discoverResponderClasses(string $namespaceName): FlattenSequence
-    {
-        $directoryURL = Bundle::main()->bundleURL->appendingPathComponent("src");
-        /** @var FlattenSequence<class-string<Responder>> */
-        return new ArrayClass([RespondersDirectory, ViewControllersDirectory])->compactMap(fn(string $directoryName): ?ArrayClass => $this->responderClassesFromDirectory($namespaceName, $directoryURL->appendingPathComponent($directoryName)))->joined();
-    }
-
-    /**
-     * @param ArrayClass<Responder> $responders
-     * @return Responder
-     */
-    private function buildResponderChain(ArrayClass $responders): Responder
-    {
-        $previous = null;
-        foreach ($responders as $responder) {
-            if ($previous) {
-                $previous->nextResponder = $responder;
-            }
-            $previous = $responder;
-        }
-        return $responders[0];
-    }
-
-    private function mergeChains(?Responder $responder1, ?Responder $responder2): ?Responder
-    {
-        if (!$responder1) {
-            return $responder2;
-        }
-        $last = $responder1;
-        while ($last->nextResponder) {
-            $last = $last->nextResponder;
-        }
-        $last->nextResponder = $responder2;
-        return $responder1;
-    }
-
-    private function customResponder(): ?Responder
-    {
-        if (!($delegate = $this->delegate)) {
-            return null;
-        }
-        $namespaceName = new ReflectionClass($delegate)->getNamespaceName();
-        $responderClasses = $this->discoverResponderClasses($namespaceName);
-        /** @var ArrayClass<Responder> $responders */
-        $responders = $responderClasses->map(
-        /**
-         * @param class-string<Responder> $responderClass
-         */
-            fn(string $responderClass): Responder => new $responderClass());
-        if ($responders->isEmpty) {
-            return null;
-        }
-        return $this->buildResponderChain($responders);
-    }
-
-    private function initialResponder(): ?Responder
-    {
-        /** @psalm-suppress InvalidArgument */
-        return $this->mergeChains($this->customResponder(), $this->buildResponderChain(new ArrayClass([$this->accessManager, new PersistentSpace(), new ResourceManager(), new Preferences(), new Uploader(), new Downloader(), new Home()])));
-    }
-
-    private function findFirstResponder(): Responder
-    {
-        $responder = $this->initialResponder();
-        while ($responder) {
-            if ($responder->isFirstResponder) {
-                return $responder;
-            }
-            $responder = $responder->nextResponder;
-        }
-        throw new NotFoundException("The requested URL was not found on this server {$this->request->url}");
-    }
-
     private function resolveFirstResponder(): Responder
     {
-        return $this->request->isPreflight ? $this : $this->findFirstResponder();
+        return new FirstResponderResolver($this, $this->accessManager, $this->delegate)->resolveFirstResponder();
     }
 
     private function initializeApplication(): void

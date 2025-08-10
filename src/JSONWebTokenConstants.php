574568dec7a8bc1355052e6052ccf6cefef6036b
dantesabatier <dantesabatier@me.com>
2025-08-09 19:10:47 -0600

    Update JSONWebTokenConstants.php

--- src/JSONWebTokenConstants.php
+++ src/JSONWebTokenConstants.php
@@ -5,3 +5,5 @@ namespace Sabatier\Service;
 const JWTPrivateKeyPreferenceKey = "JWTPrivateKeyPreferenceKey";
 const JWTValidityTimeIntervalPreferenceKey = "JWTValidityTimeIntervalPreferenceKey";
 const JWTSignatureAlgorithmPreferenceKey = "JWTSignatureAlgorithmPreferenceKey";
+const JWTComponentDelimiter = ".";
+const JWTComponentCount = 3;

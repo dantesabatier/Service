0927bd69f3069a604f6d5371995a2200df0f9a10
dantesabatier <dantesabatier@me.com>
2025-09-25 13:52:35 -0600

    Update PersistentSpace.php

--- src/PersistentSpace.php
+++ src/PersistentSpace.php
@@ -9,7 +9,7 @@ use Sabatier\CoreData\EntityDescription;
 /** @internal */
 class PersistentSpace extends Responder
 {
-    public EntityDescription $entity {
+    private(set) EntityDescription $entity {
         get => $this->entity ??= $this->managedObjectContext->persistentStoreCoordinator?->managedObjectModel?->entitiesByName?->valueForKey($this->request->url->lastPathComponent) ?? throw new NotFoundException("The requested entity \"{$this->request->url->lastPathComponent}\" does not exist.");
     }
     public bool $isFirstResponder {

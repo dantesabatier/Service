8a86c200eca381f7ae5b29c08329730a12d7a582
dantesabatier <dantesabatier@me.com>
2025-09-25 09:29:52 -0600

    Update PersistentSpaceResponseStrategyResolver.php

--- src/PersistentSpaceResponseStrategyResolver.php
+++ src/PersistentSpaceResponseStrategyResolver.php
@@ -10,10 +10,13 @@ class PersistentSpaceResponseStrategyResolver extends ResponseStrategyResolver
     public function __construct(Responder $responder)
     {
         parent::__construct($responder);
-        $this->byHTTPMethodResponseStrategyClassesTable[HTTPRequestMethod::get] = PersistentSpaceGetResponseStrategy::class;
-        $this->byHTTPMethodResponseStrategyClassesTable[HTTPRequestMethod::post] = PersistentSpacePostResponseStrategy::class;
-        $this->byHTTPMethodResponseStrategyClassesTable[HTTPRequestMethod::patch] = PersistentSpacePatchResponseStrategy::class;
-        $this->byHTTPMethodResponseStrategyClassesTable[HTTPRequestMethod::delete] = PersistentSpaceDeleteResponseStrategy::class;
-        $this->byHTTPMethodResponseStrategyClassesTable[HTTPRequestMethod::options] = DefaultResponseStrategy::class;
+        $this->byHTTPMethodResponseStrategyClassesTable[$responder->request->httpMethod] = match ($responder->request->httpMethod) {
+            HTTPRequestMethod::get => PersistentSpaceGetResponseStrategy::class,
+            HTTPRequestMethod::post => PersistentSpacePostResponseStrategy::class,
+            HTTPRequestMethod::patch => PersistentSpacePatchResponseStrategy::class,
+            HTTPRequestMethod::delete => PersistentSpaceDeleteResponseStrategy::class,
+            HTTPRequestMethod::options => DefaultResponseStrategy::class,
+            default => throw new MethodNotAllowedException(),
+        };
     }
 }

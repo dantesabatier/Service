35969052ef08ed11e643f98f48715bd62f4c398d
dantesabatier <dantesabatier@me.com>
2025-09-26 22:53:46 -0600

    Update PublicAccessPolicy.php

--- src/PublicAccessPolicy.php
+++ src/PublicAccessPolicy.php
@@ -4,6 +4,11 @@ namespace Sabatier\Service;
 
 use Override;
 
+/**
+ * A policy that defines public access to resources bypassing checks and enforcement of protected content.
+ *
+ * Extends the AccessPolicy base class to provide implementation where all requests are allowed unrestricted access.
+ */
 final class PublicAccessPolicy extends AccessPolicy
 {
     #[Override]

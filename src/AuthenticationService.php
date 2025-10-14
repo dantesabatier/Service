3932d14a6991da2aeba107e8eceb4ba4fb2cc481
dantesabatier <dantesabatier@me.com>
2025-10-14 10:22:54 -0600

    Update AuthenticationService.php

--- src/AuthenticationService.php
+++ src/AuthenticationService.php
@@ -10,10 +10,10 @@ interface AuthenticationService
     /**
      * Finds an Authorizable object based on the provided context, username, and optional serialization data.
      *
-     * @param ManagedObjectContext $context The context in which the object is managed.
      * @param string $username The username to identify the desired object.
      * @param Dictionary|null $serialization Optional additional serialized data for the search.
+     * @param ManagedObjectContext $context The context in which the object is managed.
      * @return Authorizable|null Returns an Authorizable object if found, or null otherwise.
      */
-    public function find(ManagedObjectContext $context, string $username, ?Dictionary $serialization = null): ?Authorizable;
+    public function find(string $username, ?Dictionary $serialization, ManagedObjectContext $context): ?Authorizable;
 }

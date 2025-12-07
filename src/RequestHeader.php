61f42accb7f9dd0eace04e9a0c483fa48f13333a
dantesabatier <dantesabatier@me.com>
2025-12-07 17:08:17 -0600

    Update RequestHeader.php

--- src/RequestHeader.php
+++ src/RequestHeader.php
@@ -7,14 +7,22 @@ use Sabatier\Foundation\Dictionary;
 /**
  * Represents an HTTP request header with its name and value extracted from a raw input string.
  */
-readonly class RequestHeader
+class RequestHeader
 {
     /** @var string $name The name of the authentication parameter */
-    public string $name;
+    public readonly string $name;
     /** @var string $value The value of the authentication parameter */
-    public string $value;
+    public readonly string $value;
     /** @var Dictionary<string> */
-    public Dictionary $parameters;
+    private(set) Dictionary $parameters {
+        get {
+            if (!isset($this->parameters)) {
+                preg_match_all("/(username|uri|nonce|nc|cnonce|qop|algorithm|response|opaque)=['\"]?([^'\",]+)/", $this->value, $matches);
+                $this->parameters = new Dictionary(array_combine($matches[1], $matches[2]));
+            }
+            return $this->parameters;
+        }
+    }
 
     /**
      * @param string $rawValue The raw value of the authentication parameter
@@ -28,7 +36,5 @@ readonly class RequestHeader
         [$name, $value] = $components;
         $this->name = $name;
         $this->value = $value;
-        preg_match_all("/(username|uri|nonce|nc|cnonce|qop|algorithm|response|opaque)=['\"]?([^'\",]+)/", $this->value, $matches);
-        $this->parameters = new Dictionary(array_combine($matches[1], $matches[2]));
     }
 }

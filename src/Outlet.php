f8b65fb614f24b0b6832ec2d7adcfecd0bcc02c2
dantesabatier <dantesabatier@me.com>
2025-12-12 13:25:31 -0600

    Document Outlet attribute
    Adds professional documentation for the Outlet attribute,
    explaining that it marks a property as visible to the view
    controller context.

--- src/Outlet.php
+++ src/Outlet.php
@@ -5,7 +5,12 @@ namespace Sabatier\Service;
 use Attribute;
 
 /**
- * An attribute to mark a property as visible to the view controller context.
+ * Marks a property as accessible from the view controller context.
+ *
+ * Properties annotated with this attribute are intended to be connected to
+ * interface elements or resources within a view. This allows the framework
+ * to automatically bind UI components to the corresponding properties in
+ * the controller.
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 final class Outlet

b43623d5e06745281674d94f04caba1af144ccc0
dantesabatier <dantesabatier@me.com>
2025-12-20 10:07:24 -0600

    Add detailed descriptions for `AuthorizationType` enum values

--- src/AuthorizationType.php
+++ src/AuthorizationType.php
@@ -4,12 +4,19 @@ namespace Sabatier\Service;
 
 /**
  * Represents types of authorization for various actions.
+ *
+ * Each value corresponds to an action that can be authorized on a resource.
  */
 enum AuthorizationType: int
 {
+    /** Permission to read a resource, typically used for GET and HEAD HTTP methods. */
     case read = 0;
+    /** Permission to create a resource, typically used for POST HTTP method. */
     case create = 1;
+    /** Permission to update a resource, typically used for PUT and PATCH HTTP methods. */
     case update = 2;
+    /** Permission to delete a resource, typically used for DELETE HTTP method. */
     case delete = 3;
+    /** Wildcard representing “any type of authorization”, used in generic queries or filters. Positive high value to avoid collision with real permissions. */
     case any = 99;
 }

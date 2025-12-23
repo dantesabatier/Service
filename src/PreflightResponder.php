a2152b03f1a1a1f051a647145ecbe9abe1eab13b
dantesabatier <dantesabatier@me.com>
2025-12-23 17:39:21 -0600

    Remove unused `HTTPStatusCode::noContent` in `PreflightResponder` for cleanup

--- src/PreflightResponder.php
+++ src/PreflightResponder.php
@@ -2,13 +2,11 @@
 
 namespace Sabatier\Service;
 
-use Sabatier\Foundation\Networking\HTTPStatusCode;
-
 /** @internal */
 class PreflightResponder extends Responder
 {
     public Response $response {
-        get => new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($this->request->url, HTTPStatusCode::noContent))->response, $this->request, $this->corsPolicy)->response;
+        get => new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($this->request->url))->response, $this->request, $this->corsPolicy)->response;
     }
 
     public function respondToPreflightIfNeeded(): void

355e127170dc2ebcab0119807bcf982d8193dd41
dantesabatier <dantesabatier@me.com>
2025-12-27 01:11:17 -0600

    Refactor ResourceManager response logic and headers
    Removed 'Authorization' from allowed headers and added 'isProtectedContentAvailable' property. Simplified the get response logic by removing try/finally and session management, and streamlined content type and cache control header handling.

--- src/ResourceManager.php
+++ src/ResourceManager.php
@@ -22,8 +22,9 @@ final class ResourceManager extends Responder
     }
     /** @var ArrayClass<string> */
     public ArrayClass $allowedHeaders {
-        get => new ArrayClass(["Content-Type", "Authorization"]);
+        get => new ArrayClass(["Content-Type"]);
     }
+    public bool $isProtectedContentAvailable = true;
     public URL $resourceURL {
         get => $this->resourceURL ??= new URL($this->request->url->path, FileManager::default()->documentRootDirectory)->absoluteURL;
     }
@@ -38,37 +39,32 @@ final class ResourceManager extends Responder
          * @throws Exception
          */
         get {
-            try {
-                $this->session->start();
-                $body = null;
-                $request = $this->request;
-                $this->allowedMethods->containsElement($request->httpMethod) ?: throw new MethodNotAllowedException();
-                $headers = new Dictionary();
-                $resourceURL = $this->resourceURL;
-                $path = $resourceURL->path;
-                $pathExtension = $resourceURL->pathExtension;
-                $contentType = URLFileTypeMappings::shared()->mimeType($pathExtension);
-                if (FileManager::default()->isReadableFile($path)) {
-                    $content = FileManager::default()->contents($path) ?? throw new InternalServerErrorException();
-                    if ($contentType && ($encoding = mb_detect_encoding($content))) {
-                        $contentType .= "; charset=$encoding";
-                    }
-                    $headers["Content-Type"] = $contentType;
-                    if ($request->httpMethod === HTTPRequestMethod::get) {
-                        $body = $content;
-                    }
-                } elseif (!$this->staticResourceDisposition->allowEmptyResponse) {
-                    throw new NotFoundException();
-                } else {
-                    $headers["Content-Type"] = $contentType;
+            $body = null;
+            $request = $this->request;
+            $this->allowedMethods->containsElement($request->httpMethod) ?: throw new MethodNotAllowedException();
+            $headers = new Dictionary();
+            $resourceURL = $this->resourceURL;
+            $path = $resourceURL->path;
+            $pathExtension = $resourceURL->pathExtension;
+            $contentType = URLFileTypeMappings::shared()->mimeType($pathExtension);
+            if (FileManager::default()->isReadableFile($path)) {
+                $content = FileManager::default()->contents($path) ?? throw new InternalServerErrorException();
+                if ($contentType && ($encoding = mb_detect_encoding($content))) {
+                    $contentType .= "; charset=$encoding";
                 }
-                if ($this->staticResourceDisposition->cacheable) {
-                    $headers["Cache-Control"] = "public, max-age=31536000, s-maxage=31536000, immutable";
+                $headers["Content-Type"] = $contentType;
+                if ($request->httpMethod === HTTPRequestMethod::get) {
+                    $body = $content;
                 }
-                return new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($request->url, HTTPStatusCode::ok, $headers, $body))->response, $request, $this->corsPolicy)->response;
-            } finally {
-                $this->session->commit();
+            } elseif (!$this->staticResourceDisposition->allowEmptyResponse) {
+                throw new NotFoundException();
+            } else {
+                $headers["Content-Type"] = $contentType;
             }
+            if ($this->staticResourceDisposition->cacheable) {
+                $headers["Cache-Control"] = "public, max-age=31536000, s-maxage=31536000, immutable";
+            }
+            return new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($request->url, HTTPStatusCode::ok, $headers, $body))->response, $request, $this->corsPolicy)->response;
         }
     }
 }

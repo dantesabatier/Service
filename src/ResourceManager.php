a5c5a7e198daabd567621a398e38e807fe90bf6e
dantesabatier <dantesabatier@me.com>
2026-03-14 21:01:47 -0600

    Refactor response handling in ResourceManager class
    - Simplify the response generation logic by removing unnecessary try-finally blocks and streamlining the content type and body assignment.
    - Enhance readability and maintainability of the code while ensuring proper handling of HTTP methods and resource availability.
    - Maintain session management consistency by ensuring sessions are started and committed appropriately.

--- src/ResourceManager.php
+++ src/ResourceManager.php
@@ -46,41 +46,32 @@ final class ResourceManager extends Responder
          * @throws Exception
          */
         get {
-            try {
-                $body = null;
-                $request = $this->request;
-                $this->allowedMethods->containsElement($request->httpMethod) ?: throw new MethodNotAllowedException();
-                if ($this->isSessionEnabled) {
-                    $this->session->start();
+            $body = null;
+            $request = $this->request;
+            $this->allowedMethods->containsElement($request->httpMethod) ?: throw new MethodNotAllowedException();
+            $headers = new Dictionary();
+            $resourceURL = $this->resourceURL;
+            $path = $resourceURL->path;
+            $pathExtension = $resourceURL->pathExtension;
+            $contentType = URLFileTypeMappings::shared()->mimeType($pathExtension);
+            if (FileManager::default()->isReadableFile($path)) {
+                $content = FileManager::default()->contents($path) ?? throw new InternalServerErrorException();
+                if ($contentType && ($encoding = mb_detect_encoding($content))) {
+                    $contentType .= "; charset=$encoding";
                 }
-                $headers = new Dictionary();
-                $resourceURL = $this->resourceURL;
-                $path = $resourceURL->path;
-                $pathExtension = $resourceURL->pathExtension;
-                $contentType = URLFileTypeMappings::shared()->mimeType($pathExtension);
-                if (FileManager::default()->isReadableFile($path)) {
-                    $content = FileManager::default()->contents($path) ?? throw new InternalServerErrorException();
-                    if ($contentType && ($encoding = mb_detect_encoding($content))) {
-                        $contentType .= "; charset=$encoding";
-                    }
-                    $headers["Content-Type"] = $contentType;
-                    if ($request->httpMethod === HTTPRequestMethod::get) {
-                        $body = $content;
-                    }
-                } elseif (!$this->staticResourceDisposition->allowEmptyResponse) {
-                    throw new NotFoundException();
-                } else {
-                    $headers["Content-Type"] = $contentType;
+                $headers["Content-Type"] = $contentType;
+                if ($request->httpMethod === HTTPRequestMethod::get) {
+                    $body = $content;
                 }
-                if ($this->staticResourceDisposition->cacheable) {
-                    $headers["Cache-Control"] = "public, max-age=31536000, s-maxage=31536000, immutable";
-                }
-                return new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($request->url, $this->statusCode, $headers, $body))->response, $request, $this->corsPolicy)->response;
-            } finally {
-                if ($this->isSessionEnabled) {
-                    $this->session->commit();
-                }
+            } elseif (!$this->staticResourceDisposition->allowEmptyResponse) {
+                throw new NotFoundException();
+            } else {
+                $headers["Content-Type"] = $contentType;
             }
+            if ($this->staticResourceDisposition->cacheable) {
+                $headers["Cache-Control"] = "public, max-age=31536000, s-maxage=31536000, immutable";
+            }
+            return new CORSResponseDecorator(new ResponseHeaderSanitizerDecorator(new Response($request->url, $this->statusCode, $headers, $body))->response, $request, $this->corsPolicy)->response;
         }
     }
 }

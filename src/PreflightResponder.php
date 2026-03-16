aaae530e6e9606f051ee190374d7206f28836fdb
dantesabatier <dantesabatier@me.com>
2026-03-15 20:46:36 -0600

    Enhance PreflightResponder to include allowed HTTP methods
    - Introduce a new property to define allowed HTTP methods using ArrayClass, improving clarity and maintainability.
    - Update the PreflightResponder class to utilize the new allowedMethods property for handling preflight requests.

--- src/PreflightResponder.php
+++ src/PreflightResponder.php
@@ -2,9 +2,17 @@
 
 namespace Sabatier\Service;
 
+use Sabatier\Foundation\ArrayClass;
+use Sabatier\Foundation\Networking\HTTPRequestMethod;
+
 /** @internal */
 final class PreflightResponder extends Responder
 {
+    /** @var ArrayClass<string> */
+    protected ArrayClass $allowedMethods {
+        get => new ArrayClass([HTTPRequestMethod::options]);
+    }
+
     public function respondToPreflightIfNeeded(): void
     {
         if (!$this->request->isPreflight) {

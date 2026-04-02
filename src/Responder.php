a2e49101d246e397ae42eff91bcc404f411c888b
dantesabatier <dantesabatier@me.com>
2026-04-02 11:56:42 -0600

    Refactor Responder class to improve response handling with CORS support
    - Updated the response generation logic to directly return a CORSResponseDecorator, enhancing the handling of CORS policies in responses.
    - Simplified the response creation process by integrating the decorator application into a single return statement, improving code clarity.

--- src/Responder.php
+++ src/Responder.php
@@ -115,14 +115,13 @@ abstract class Responder extends ObjectClass
                     } && ($selector = $this->selector)) {
                     $this->perform($selector);
                 }
-                $response = $this->decorators->reduce(new Response($request->url, $this->statusCode, body: $this->data),
+                return new CORSResponseDecorator($this->decorators->reduce(new Response($request->url, $this->statusCode, body: $this->data),
                     /**
                      * @param Response $response
                      * @param class-string<ResponseDecorator> $decorator
                      * @return Response
                      */
-                    fn(Response $response, string $decorator): Response => new $decorator($response)->response);
-                return new CORSResponseDecorator($response, $request, $this->corsPolicy)->response;
+                    fn(Response $response, string $decorator): Response => new $decorator($response)->response), $request, $this->corsPolicy)->response;
             } finally {
                 if ($this->isSessionEnabled) {
                     $this->session->commit();

1c8d746c91fbd853cb1cfca111e6ecacd593a11e
dantesabatier <dantesabatier@me.com>
2026-04-15 13:01:05 -0600

    Update SecurityHeadersPolicy.php

--- src/SecurityHeadersPolicy.php
+++ src/SecurityHeadersPolicy.php
@@ -7,13 +7,13 @@ use Sabatier\Foundation\ProcessInfo;
 /** @internal */
 final readonly class SecurityHeadersPolicy
 {
-    public function __construct(public ?string $contentSecurityPolicy = null, public ?string $strictTransportSecurity = null, public string $xContentTypeOptions = SecurityXContentTypeOptionsDefault, public string $xFrameOptions = SecurityXFrameOptionsDefault, public string $referrerPolicy = SecurityReferrerPolicyDefault, public string $permissionsPolicy = SecurityPermissionsPolicyDefault)
+    public function __construct(public ?string $contentSecurityPolicy = null, public ?string $strictTransportSecurity = null, public ?string $xContentTypeOptions = null, public ?string $xFrameOptions = null, public ?string $referrerPolicy = null, public ?string $permissionsPolicy = null)
     {
     }
 
     public static function policy(): SecurityHeadersPolicy
     {
         $environment = ProcessInfo::processInfo()->environment;
-        return new SecurityHeadersPolicy($environment[SecurityContentSecurityPolicyKey], $environment[SecurityStrictTransportSecurityKey], [SecurityXContentTypeOptionsKey] ?? SecurityXContentTypeOptionsDefault, $environment[SecurityXFrameOptionsKey] ?? SecurityXFrameOptionsDefault, $environment[SecurityReferrerPolicyKey] ?? SecurityReferrerPolicyDefault, $environment[SecurityPermissionsPolicyKey] ?? SecurityPermissionsPolicyDefault);
+        return new SecurityHeadersPolicy($environment[SecurityContentSecurityPolicyKey], $environment[SecurityStrictTransportSecurityKey], $environment[SecurityXContentTypeOptionsKey], $environment[SecurityXFrameOptionsKey], $environment[SecurityReferrerPolicyKey], $environment[SecurityPermissionsPolicyKey]);
     }
 }

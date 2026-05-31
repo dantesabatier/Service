f6d04808a652202b76c5501ee6a2332d1fc9fc8d
dantesabatier <dantesabatier@me.com>
2026-05-31 12:05:44 -0600

    Update RateLimitPolicy.php

--- src/RateLimitPolicy.php
+++ src/RateLimitPolicy.php
@@ -48,7 +48,7 @@ final readonly class RateLimitPolicy
     public static function policy(): RateLimitPolicy
     {
         $environment = ProcessInfo::processInfo()->environment;
-        $proxies = string_split_trimmed((string)$environment[RateLimitTrustedProxiesKey] ?? "");
+        $proxies = string_split_trimmed((string)$environment[RateLimitTrustedProxiesKey]);
         return new RateLimitPolicy(filter_var($environment[RateLimitEnabledKey] ?? RateLimitEnabledDefault, FILTER_VALIDATE_BOOL), (int)($environment[RateLimitMaxRequestsUserKey] ?? RateLimitMaxRequestsUserDefault), (int)($environment[RateLimitMaxRequestsIPKey] ?? RateLimitMaxRequestsIPDefault), (int)($environment[RateLimitWindowSecondsKey] ?? RateLimitWindowSecondsDefault), $proxies);
     }
 }
